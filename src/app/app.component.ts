d3083c613722baa9b7c30b662e6fc1bbc5027bb7
Sruti Dash <sruti.dash1203@gmail.com>
2024-07-09 00:49:48 +0530

    added notifications and bulk notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'notification-template';
+// }
+
+// export class AppComponent {
+//   dropdownOpen = false;
+//   activeMenu: string | null = null;
+//   activeSubMenu: string | null = null;
+
+//   toggleDropdown() {
+//     this.dropdownOpen = !this.dropdownOpen;
+//   }
+
+//   toggleSubMenu(menu: string) {
+//     this.activeMenu = this.activeMenu === menu ? null : menu;
+//   }
+
+//   navigateTo(route: string) {
+//     // Logic to navigate to the desired route
+//     // For example, using Angular Router
+//     this.router.navigate([route]);
+//   }
+
+//   logout() {
+//     // Logic to handle logout
+//   }
+// }
+
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'notification-template';
+  dropdownOpen = false;
+  activeMenu: string | null = null;
+  activeSubMenu: string | null = null;
+
+  constructor(private router: Router) {}
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  toggleSubMenu(menu: string) {
+    this.activeMenu = this.activeMenu === menu ? null : menu;
+  }
+
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
+
+  logout() {
+    // Logic to handle logout
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [
+//     provideClientHydration()
+//   ],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,11 +30,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      // Your routes here
+    ])
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
