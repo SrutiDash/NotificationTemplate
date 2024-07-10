58ed3ae4c72aa271578e123362b078b317774b60
Sruti Dash <sruti.dash1203@gmail.com>
2024-07-10 21:31:21 +0530

    removed commented codes

--- src/app/notification/create-notification/create-notification.component.ts
+++ src/app/notification/create-notification/create-notification.component.ts
@@ -1,25 +1,3 @@
-
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-create-notification',
-//   templateUrl: './create-notification.component.html',
-//   styleUrls: ['./create-notification.component.css']
-// })
-// export class CreateNotificationComponent {
-//   selectedServiceType: string = '';
-//   isServiceTypeSelected: boolean = false;
-//   searchParameters = [
-//     { channel: 'Email', language: 'English', header: 'Header 1', body: 'Body 1', attachable: 'Yes', documents: 'Doc 1' },
-//     { channel: 'SMS', language: 'French', header: 'Header 2', body: 'Body 2', attachable: 'No', documents: 'Doc 2' },
-//     // Add more rows as needed
-//   ];
-
-//   onServiceTypeChange(event: any) {
-//     this.isServiceTypeSelected = !!event.target.value;
-//   }
-// }
-
 import { Component } from '@angular/core';
 
 @Component({
