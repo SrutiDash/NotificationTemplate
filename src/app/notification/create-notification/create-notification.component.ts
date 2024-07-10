fb6f714c3aa4d410a3d9e045fb6c2654714a006e
Sruti Dash <sruti.dash1203@gmail.com>
2024-07-10 17:35:54 +0530

    added search parameter and added column names. need to add rows for it

--- src/app/notification/create-notification/create-notification.component.ts
+++ src/app/notification/create-notification/create-notification.component.ts
@@ -1,3 +1,72 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-create-notification',
+//   templateUrl: './create-notification.component.html',
+//   styleUrls: ['./create-notification.component.css']
+// })
+// export class CreateNotificationComponent { }
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-create-notification',
+//   templateUrl: './create-notification.component.html',
+//   styleUrls: ['./create-notification.component.css']
+// })
+// export class CreateNotificationComponent {
+//   selectedServiceType: string | null = null;
+//   selectedEventTrigger: string | null = null;
+//   selectedParty: string | null = null;
+//   serviceTypes = [
+//     'Send Money',
+//     'Cash - In',
+//     'Add Money',
+//     'Transfer To Bank',
+//     'Recharge',
+//     'Bill Pay',
+//     'Request Money'
+//   ];
+
+//   onServiceTypeChange() {
+//     console.log(this.selectedServiceType);
+//   }
+// }
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-create-notification',
+//   templateUrl: './create-notification.component.html',
+//   styleUrls: ['./create-notification.component.css']
+// })
+// export class CreateNotificationComponent {
+//   selectedServiceType: string = '';
+//   selectedEventTrigger: string = '';
+//   selectedParty: string = '';
+//   isServiceTypeSelected: boolean = false;
+
+//   onServiceTypeChange() {
+//     this.isServiceTypeSelected = !!this.selectedServiceType;
+//   }
+// }
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-create-notification',
+//   templateUrl: './create-notification.component.html',
+//   styleUrls: ['./create-notification.component.css']
+// })
+// export class CreateNotificationComponent {
+//   selectedServiceType: string = '';
+//   isServiceTypeSelected: boolean = false;
+
+//   onServiceTypeChange(event: any) {
+//     this.isServiceTypeSelected = !!event.target.value;
+//   }
+// }
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,5 +74,17 @@ import { Component } from '@angular/core';
   templateUrl: './create-notification.component.html',
   styleUrls: ['./create-notification.component.css']
 })
-export class CreateNotificationComponent { }
+export class CreateNotificationComponent {
+  selectedServiceType: string = '';
+  isServiceTypeSelected: boolean = false;
+  searchParameters = [
+    { channel: 'Email', language: 'English', header: 'Header 1', body: 'Body 1', attachable: 'Yes', documents: 'Doc 1' },
+    { channel: 'SMS', language: 'French', header: 'Header 2', body: 'Body 2', attachable: 'No', documents: 'Doc 2' },
+    // Add more rows as needed
+  ];
+
+  onServiceTypeChange(event: any) {
+    this.isServiceTypeSelected = !!event.target.value;
+  }
+}
 
