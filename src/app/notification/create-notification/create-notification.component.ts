56818a8861a80a7ff2f5072f24f243cb55102ee9
Sruti Dash <sruti.dash1203@gmail.com>
2024-07-10 21:28:25 +0530

    added the columns, rows and dropdown in attachabledocumnet

--- src/app/notification/create-notification/create-notification.component.ts
+++ src/app/notification/create-notification/create-notification.component.ts
@@ -1,4 +1,25 @@
 
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-create-notification',
+//   templateUrl: './create-notification.component.html',
+//   styleUrls: ['./create-notification.component.css']
+// })
+// export class CreateNotificationComponent {
+//   selectedServiceType: string = '';
+//   isServiceTypeSelected: boolean = false;
+//   searchParameters = [
+//     { channel: 'Email', language: 'English', header: 'Header 1', body: 'Body 1', attachable: 'Yes', documents: 'Doc 1' },
+//     { channel: 'SMS', language: 'French', header: 'Header 2', body: 'Body 2', attachable: 'No', documents: 'Doc 2' },
+//     // Add more rows as needed
+//   ];
+
+//   onServiceTypeChange(event: any) {
+//     this.isServiceTypeSelected = !!event.target.value;
+//   }
+// }
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,14 +30,15 @@ import { Component } from '@angular/core';
 export class CreateNotificationComponent {
   selectedServiceType: string = '';
   isServiceTypeSelected: boolean = false;
+  documents = ['Document 1', 'Document 2', 'Document 3'];
   searchParameters = [
-    { channel: 'Email', language: 'English', header: 'Header 1', body: 'Body 1', attachable: 'Yes', documents: 'Doc 1' },
-    { channel: 'SMS', language: 'French', header: 'Header 2', body: 'Body 2', attachable: 'No', documents: 'Doc 2' },
-    // Add more rows as needed
+    { channel: 'In-App Notification', language: '', header: '', body: '', attachable: '', documents: '' },
+    { channel: 'SMS', language: '', header: '', body: '', attachable: '', documents: '' },
+    { channel: 'Email', language: '', header: '', body: '', attachable: '', documents: '' },
+    { channel: 'URL', language: '', header: '', body: '', attachable: '', documents: '' }
   ];
 
   onServiceTypeChange(event: any) {
     this.isServiceTypeSelected = !!event.target.value;
   }
 }
-
