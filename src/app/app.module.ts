a2fc5c360ccf601f25f0e6db9b9aeb1b25797e27
Sruti Dash <sruti.dash1203@gmail.com>
2024-07-11 14:02:59 +0530

    modified search menu t drop down list in service type, event trigger and party. now if the user wants to choose the list of itemns, user can simply search and click upon the options and it will be autofilled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,63 +1,8 @@
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { FilterPromptComponent } from './notification/filter-prompt/filter-prompt.component';
-// import { CreateNotificationComponent } from './notification/create-notification/create-notification.component';
-// import { NotificationManagementComponent } from './notification/notification-management/notification-management.component';
-
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     FilterPromptComponent,
-//     CreateNotificationComponent,
-//     NotificationManagementComponent,
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { AppRoutingModule } from './app-routing.module';
-// import { RouterModule } from '@angular/router';
-// import { FormsModule } from '@angular/forms'; // Import FormsModule
-// import { AppComponent } from './app.component';
-// import { FilterPromptComponent } from './notification/filter-prompt/filter-prompt.component';
-// import { CreateNotificationComponent } from './notification/create-notification/create-notification.component';
-// import { NotificationManagementComponent } from './notification/notification-management/notification-management.component';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     FilterPromptComponent,
-//     CreateNotificationComponent,
-//     NotificationManagementComponent,
-//   ],
-//   imports: [
-//     AppRoutingModule,
-//     BrowserModule,
-//     RouterModule.forRoot([
-//       // Your routes here
-//     ]),
-//     FormsModule // Add FormsModule here
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { AppRoutingModule } from './app-routing.module';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { AppComponent } from './app.component';
 import { FilterPromptComponent } from './notification/filter-prompt/filter-prompt.component';
 import { CreateNotificationComponent } from './notification/create-notification/create-notification.component';
@@ -71,15 +16,14 @@ import { NotificationManagementComponent } from './notification/notification-man
     NotificationManagementComponent,
   ],
   imports: [
-    AppRoutingModule,
     BrowserModule,
-    RouterModule.forRoot([
-      // Your routes here
-    ]),
-    FormsModule // Add FormsModule here
+    AppRoutingModule,
+    FormsModule,
+    NgSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
 
--- src/app/notification/create-notification/create-notification.component.ts
+++ src/app/notification/create-notification/create-notification.component.ts
@@ -1,3 +1,60 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-create-notification',
+//   templateUrl: './create-notification.component.html',
+//   styleUrls: ['./create-notification.component.css']
+// })
+// export class CreateNotificationComponent {
+//   selectedServiceType: string = '';
+//   isServiceTypeSelected: boolean = false;
+//   documents: string[] = ['Document 1', 'Document 2', 'Document 3'];
+//   searchParameters = [
+//     { channel: 'In-App Notification', language: '', header: '', body: '', attachable: '', documents: '' },
+//     { channel: 'SMS', language: '', header: '', body: '', attachable: '', documents: '' },
+//     { channel: 'Email', language: '', header: '', body: '', attachable: '', documents: '' },
+//     { channel: 'URL', language: '', header: '', body: '', attachable: '', documents: '' }
+//   ];
+
+//   serviceTypes: string[] = [
+//     'Send Money', 'Cash - In', 'Add Money', 'Transfer To Bank', 'Recharge', 'Bill Pay', 'Request Money'
+//   ];
+
+//   eventTriggers: string[] = ['Success', 'Failed'];
+//   parties: string[] = ['Sender', 'Receiver'];
+
+//   filteredServiceTypes: string[] = [];
+//   filteredEventTriggers: string[] = [];
+//   filteredParties: string[] = [];
+
+//   onServiceTypeChange(event: any) {
+//     this.isServiceTypeSelected = !!event.target.value;
+//   }
+
+//   filterOptions(type: string, event: any) {
+//     const query = event.target.value.toLowerCase();
+//     if (type === 'serviceType') {
+//       this.filteredServiceTypes = this.serviceTypes.filter(option => option.toLowerCase().includes(query));
+//     } else if (type === 'eventTrigger') {
+//       this.filteredEventTriggers = this.eventTriggers.filter(option => option.toLowerCase().includes(query));
+//     } else if (type === 'party') {
+//       this.filteredParties = this.parties.filter(option => option.toLowerCase().includes(query));
+//     }
+//   }
+
+//   selectOption(type: string, option: string) {
+//     if (type === 'serviceType') {
+//       this.selectedServiceType = option;
+//       this.isServiceTypeSelected = true;
+//       this.filteredServiceTypes = [];
+//     } else if (type === 'eventTrigger') {
+//       // Handle event trigger selection
+//     } else if (type === 'party') {
+//       // Handle party selection
+//     }
+//   }
+// }
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,8 +64,10 @@ import { Component } from '@angular/core';
 })
 export class CreateNotificationComponent {
   selectedServiceType: string = '';
+  selectedEventTrigger: string = '';
+  selectedParty: string = '';
   isServiceTypeSelected: boolean = false;
-  documents = ['Document 1', 'Document 2', 'Document 3'];
+  documents: string[] = ['Document 1', 'Document 2', 'Document 3'];
   searchParameters = [
     { channel: 'In-App Notification', language: '', header: '', body: '', attachable: '', documents: '' },
     { channel: 'SMS', language: '', header: '', body: '', attachable: '', documents: '' },
@@ -16,7 +75,56 @@ export class CreateNotificationComponent {
     { channel: 'URL', language: '', header: '', body: '', attachable: '', documents: '' }
   ];
 
+  serviceTypes: string[] = [
+    'Send Money', 'Cash - In', 'Add Money', 'Transfer To Bank', 'Recharge', 'Bill Pay', 'Request Money'
+  ];
+
+  eventTriggers: string[] = ['Success', 'Failed'];
+  parties: string[] = ['Sender', 'Receiver'];
+
+  filteredServiceTypes: string[] = [];
+  filteredEventTriggers: string[] = [];
+  filteredParties: string[] = [];
+
   onServiceTypeChange(event: any) {
     this.isServiceTypeSelected = !!event.target.value;
+  }
+
+  filterOptions(type: string, event: any) {
+    const query = event.target.value.toLowerCase();
+    if (type === 'serviceType') {
+      this.filteredServiceTypes = this.serviceTypes.filter(option => option.toLowerCase().includes(query));
+    } else if (type === 'eventTrigger') {
+      this.filteredEventTriggers = this.eventTriggers.filter(option => option.toLowerCase().includes(query));
+    } else if (type === 'party') {
+      this.filteredParties = this.parties.filter(option => option.toLowerCase().includes(query));
+    }
+  }
+
+  selectOption(type: string, option: string) {
+    if (type === 'serviceType') {
+      this.selectedServiceType = option;
+      this.isServiceTypeSelected = true;
+      this.filteredServiceTypes = [];
+      (document.getElementById('serviceTypeSearch') as HTMLInputElement).value = option;
+    } else if (type === 'eventTrigger') {
+      this.selectedEventTrigger = option;
+      this.filteredEventTriggers = [];
+      (document.getElementById('eventTriggerSearch') as HTMLInputElement).value = option;
+    } else if (type === 'party') {
+      this.selectedParty = option;
+      this.filteredParties = [];
+      (document.getElementById('partySearch') as HTMLInputElement).value = option;
+    }
+  }
+
+  showOptions(type: string) {
+    if (type === 'serviceType') {
+      this.filteredServiceTypes = [...this.serviceTypes];
+    } else if (type === 'eventTrigger') {
+      this.filteredEventTriggers = [...this.eventTriggers];
+    } else if (type === 'party') {
+      this.filteredParties = [...this.parties];
+    }
   }
 }
