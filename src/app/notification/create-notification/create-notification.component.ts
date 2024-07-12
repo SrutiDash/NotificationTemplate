3c4bc87821deaac1a2de3548a2b38036da051f99
Sruti Dash <sruti.dash1203@gmail.com>
2024-07-12 21:26:57 +0530

    added database connectivity of service type which is code column in service_code table

--- src/app/notification/create-notification/create-notification.component.ts
+++ src/app/notification/create-notification/create-notification.component.ts
@@ -15,10 +15,10 @@ export class CreateNotificationComponent implements OnInit {
   isConditionSelected: boolean = false;
   documents: string[] = ['Document 1', 'Document 2', 'Document 3'];
   searchParameters = [
-    { channel: 'In-App Notification', language: '', header: '', body: '', attachable: '', documents: '' },
-    { channel: 'SMS', language: '', header: '', body: '', attachable: '', documents: '' },
-    { channel: 'Email', language: '', header: '', body: '', attachable: '', documents: '' },
-    { channel: 'URL', language: '', header: '', body: '', attachable: '', documents: '' }
+    { channel: '', language: '', header: '', body: '', attachable: '', documents: '', filteredChannels: [] },
+    { channel: '', language: '', header: '', body: '', attachable: '', documents: '', filteredChannels: [] },
+    { channel: '', language: '', header: '', body: '', attachable: '', documents: '', filteredChannels: [] },
+    { channel: '', language: '', header: '', body: '', attachable: '', documents: '', filteredChannels: [] }
   ];
 
   serviceTypes: string[] = [];
@@ -32,6 +32,7 @@ export class CreateNotificationComponent implements OnInit {
   conditionFields2: string[] = ['Field2 Option 1', 'Field2 Option 2', 'Field2 Option 3'];
   conditionFields3: string[] = ['Field3 Option 1', 'Field3 Option 2', 'Field3 Option 3'];
   conditionFields4: string[] = ['Field4 Option 1', 'Field4 Option 2', 'Field4 Option 3'];
+  channels: string[] = ['In-App Notification', 'SMS', 'Email', 'URL'];
 
   filteredConditions: string[] = [];
   filteredConditionFields1: string[][] = [];
@@ -39,16 +40,11 @@ export class CreateNotificationComponent implements OnInit {
   filteredConditionFields3: string[][] = [];
   filteredConditionFields4: string[][] = [];
 
+  addedConditions: string[] = [];
+
   showServiceTypeOptions: boolean = false;
   showEventTriggerOptions: boolean = false;
   showPartyOptions: boolean = false;
-  showConditionOptions: boolean = false;
-  showConditionFieldOptions1: boolean[] = [];
-  showConditionFieldOptions2: boolean[] = [];
-  showConditionFieldOptions3: boolean[] = [];
-  showConditionFieldOptions4: boolean[] = [];
-
-  addedConditions: string[] = [];
 
   constructor(private dataService: DataService) { }
 
@@ -69,7 +65,7 @@ export class CreateNotificationComponent implements OnInit {
     });
   }
 
-  filterOptions(type: string, event: any, index?: number) {
+  filterOptions(type: string, event: any, param?: any, index?: number) {
     const query = event.target.value.toLowerCase();
     if (type === 'serviceType') {
       this.filteredServiceTypes = this.serviceTypes.filter(option => option.toLowerCase().includes(query));
@@ -87,62 +83,58 @@ export class CreateNotificationComponent implements OnInit {
       this.filteredConditionFields3[index] = this.conditionFields3.filter(option => option.toLowerCase().includes(query));
     } else if (type === 'conditionField4' && index !== undefined) {
       this.filteredConditionFields4[index] = this.conditionFields4.filter(option => option.toLowerCase().includes(query));
+    } else if (type === 'channel' && param) {
+      param.filteredChannels = this.channels.filter(option => option.toLowerCase().includes(query));
     }
   }
 
-  selectOption(type: string, option: string, index?: number) {
+  selectOption(type: string, option: string, param?: any, index?: number) {
     if (type === 'serviceType') {
       this.selectedServiceType = option;
       this.isServiceTypeSelected = true;
-      this.filteredServiceTypes = [];
       this.showServiceTypeOptions = false;
+      this.filteredServiceTypes = [];
       (document.getElementById('serviceTypeSearch') as HTMLInputElement).value = option;
     } else if (type === 'eventTrigger') {
       this.selectedEventTrigger = option;
-      this.filteredEventTriggers = [];
       this.showEventTriggerOptions = false;
+      this.filteredEventTriggers = [];
       (document.getElementById('eventTriggerSearch') as HTMLInputElement).value = option;
     } else if (type === 'party') {
       this.selectedParty = option;
-      this.filteredParties = [];
       this.showPartyOptions = false;
+      this.filteredParties = [];
       (document.getElementById('partySearch') as HTMLInputElement).value = option;
     } else if (type === 'condition') {
       this.selectedCondition = option;
       this.isConditionSelected = true;
       this.filteredConditions = [];
-      this.showConditionOptions = false;
       this.addedConditions.push(option);
       this.filteredConditionFields1.push([]);
       this.filteredConditionFields2.push([]);
       this.filteredConditionFields3.push([]);
       this.filteredConditionFields4.push([]);
-      this.showConditionFieldOptions1.push(false);
-      this.showConditionFieldOptions2.push(false);
-      this.showConditionFieldOptions3.push(false);
-      this.showConditionFieldOptions4.push(false);
       (document.getElementById('conditionSearch') as HTMLInputElement).value = option;
     } else if (type === 'conditionField1' && index !== undefined) {
       this.filteredConditionFields1[index] = [];
-      this.showConditionFieldOptions1[index] = false;
       (document.getElementById('conditionField1_' + index) as HTMLInputElement).value = option;
     } else if (type === 'conditionField2' && index !== undefined) {
       this.filteredConditionFields2[index] = [];
-      this.showConditionFieldOptions2[index] = false;
       (document.getElementById('conditionField2_' + index) as HTMLInputElement).value = option;
     } else if (type === 'conditionField3' && index !== undefined) {
       this.filteredConditionFields3[index] = [];
-      this.showConditionFieldOptions3[index] = false;
       (document.getElementById('conditionField3_' + index) as HTMLInputElement).value = option;
     } else if (type === 'conditionField4' && index !== undefined) {
       this.filteredConditionFields4[index] = [];
-      this.showConditionFieldOptions4[index] = false;
       (document.getElementById('conditionField4_' + index) as HTMLInputElement).value = option;
       this.addCondition(index);
+    } else if (type === 'channel' && param) {
+      param.channel = option;
+      param.filteredChannels = [];
     }
   }
 
-  showOptions(type: string, index?: number) {
+  showOptions(type: string, param?: any, index?: number) {
     if (type === 'serviceType') {
       this.showServiceTypeOptions = true;
       this.filteredServiceTypes = [...this.serviceTypes];
@@ -153,20 +145,17 @@ export class CreateNotificationComponent implements OnInit {
       this.showPartyOptions = true;
       this.filteredParties = [...this.parties];
     } else if (type === 'condition') {
-      this.showConditionOptions = true;
       this.filteredConditions = [...this.conditions];
     } else if (type === 'conditionField1' && index !== undefined) {
-      this.showConditionFieldOptions1[index] = true;
       this.filteredConditionFields1[index] = [...this.conditionFields1];
     } else if (type === 'conditionField2' && index !== undefined) {
-      this.showConditionFieldOptions2[index] = true;
       this.filteredConditionFields2[index] = [...this.conditionFields2];
     } else if (type === 'conditionField3' && index !== undefined) {
-      this.showConditionFieldOptions3[index] = true;
       this.filteredConditionFields3[index] = [...this.conditionFields3];
     } else if (type === 'conditionField4' && index !== undefined) {
-      this.showConditionFieldOptions4[index] = true;
       this.filteredConditionFields4[index] = [...this.conditionFields4];
+    } else if (type === 'channel' && param) {
+      param.filteredChannels = [...this.channels];
     }
   }
 
@@ -176,10 +165,6 @@ export class CreateNotificationComponent implements OnInit {
     this.filteredConditionFields2.push([]);
     this.filteredConditionFields3.push([]);
     this.filteredConditionFields4.push([]);
-    this.showConditionFieldOptions1.push(false);
-    this.showConditionFieldOptions2.push(false);
-    this.showConditionFieldOptions3.push(false);
-    this.showConditionFieldOptions4.push(false);
   }
 
   removeCondition(index: number) {
@@ -188,10 +173,6 @@ export class CreateNotificationComponent implements OnInit {
     this.filteredConditionFields2.splice(index, 1);
     this.filteredConditionFields3.splice(index, 1);
     this.filteredConditionFields4.splice(index, 1);
-    this.showConditionFieldOptions1.splice(index, 1);
-    this.showConditionFieldOptions2.splice(index, 1);
-    this.showConditionFieldOptions3.splice(index, 1);
-    this.showConditionFieldOptions4.splice(index, 1);
   }
 
   showOr(index: number) {
@@ -208,4 +189,3 @@ export class CreateNotificationComponent implements OnInit {
     }
   }
 }
-
