b02023e482796555a425f5a99e039d7ebee349fe
Sruti Dash <sruti.dash1203@gmail.com>
2024-07-17 19:53:59 +0530

    removed the commented codes

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,37 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class DataService {
-//   private baseUrl = 'http://localhost:5000/api';
-
-//   selectedServiceType: string = '';
-//   selectedEventTrigger: string = '';
-//   selectedParty: string = '';
-//   senderParent: string = '';
-//   grade: string = '';
-//   channels: string[] = [];
-//   inAppSmsNotifications: any[] = [];
-//   emailNotifications: any[] = [];
-
-//   constructor(private http: HttpClient) { }
-
-//   getServiceTypes(): Observable<any> {
-//     return this.http.get(`${this.baseUrl}/service-types`);
-//   }
-
-//   getEventTriggers(): Observable<any> {
-//     return this.http.get(`${this.baseUrl}/event-triggers`);
-//   }
-
-//   getParties(): Observable<any> {
-//     return this.http.get(`${this.baseUrl}/parties`);
-//   }
-// }
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
