3efddde79bac55386ec2a225091703dffd74164b
Sruti Dash <sruti.dash1203@gmail.com>
2024-07-22 12:13:06 +0530

    added create new layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,43 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
+// import { NgSelectModule } from '@ng-select/ng-select';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { FilterPromptComponent } from './notification/filter-prompt/filter-prompt.component';
+// import { CreateNotificationComponent } from './notification/create-notification/create-notification.component';
+// import { NotificationManagementComponent } from './notification/notification-management/notification-management.component';
+// import { NotificationEditComponent } from './notification/notification-edit/notification-edit.component';
+// import { ReviewConfirmComponent } from './notification/review-confirm/review-confirm.component';
+// import { BulkNotificationsComponent } from './notification/bulk-notifications/bulk-notifications.component';
+// import { FilterPipe } from './filter.pipe';
+// import { CreateBulkNotificationComponent } from './notification/create-bulk-notification/create-bulk-notification.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     FilterPromptComponent,
+//     CreateNotificationComponent,
+//     NotificationManagementComponent,
+//     NotificationEditComponent,
+//     ReviewConfirmComponent,
+//     BulkNotificationsComponent,
+//     FilterPipe,
+//     CreateBulkNotificationComponent,
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     FormsModule,
+//     NgSelectModule,
+//     HttpClientModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -37,3 +77,4 @@ import { CreateBulkNotificationComponent } from './notification/create-bulk-noti
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
--- src/app/notification/create-bulk-notification/create-bulk-notification.component.ts
+++ src/app/notification/create-bulk-notification/create-bulk-notification.component.ts
@@ -1,5 +1,196 @@
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-create-bulk-notification',
+//   templateUrl: './create-bulk-notification.component.html',
+//   styleUrls: ['./create-bulk-notification.component.css']
+// })
+// export class CreateBulkNotificationComponent implements OnInit {
+//   channels = [
+//     { name: 'In-App Notification' },
+//     { name: 'SMS' },
+//     { name: 'Email' }
+//   ];
+
+//   constructor(private router: Router) {}
+
+//   ngOnInit(): void {}
+
+//   cancel(): void {
+//     this.router.navigate(['/bulk-notifications']);
+//   }
+
+//   next(): void {
+//     // Implement the next step logic here
+//   }
+// }
+
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-create-bulk-notification',
+//   templateUrl: './create-bulk-notification.component.html',
+//   styleUrls: ['./create-bulk-notification.component.css']
+// })
+// export class CreateBulkNotificationComponent implements OnInit {
+//   channels = [
+//     { id: 1, name: 'In-App Notification' },
+//     { id: 2, name: 'SMS' },
+//     { id: 3, name: 'Email' }
+//   ];
+
+//   selectedChannels = [];
+
+//   constructor(private router: Router) {}
+
+//   ngOnInit(): void {}
+
+//   cancel(): void {
+//     this.router.navigate(['/bulk-notifications']);
+//   }
+
+//   next(): void {
+//     // Implement the next step logic here
+//   }
+// }
+
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+
+// interface Channel {
+//   name: string;
+//   language?: string;
+//   messageHeader?: string;
+//   messageBody?: string;
+//   image?: File;
+// }
+
+// @Component({
+//   selector: 'app-create-bulk-notification',
+//   templateUrl: './create-bulk-notification.component.html',
+//   styleUrls: ['./create-bulk-notification.component.css']
+// })
+// export class CreateBulkNotificationComponent implements OnInit {
+//   channels = [
+//     { id: 1, name: 'In-App Notification' },
+//     { id: 2, name: 'SMS' },
+//     { id: 3, name: 'Email' }
+//   ];
+
+//   selectedChannels: Channel[] = [
+//     { name: 'In-App Notification' },
+//     { name: 'SMS' },
+//     { name: 'Email' }
+//   ];
+
+//   constructor(private router: Router) {}
+
+//   ngOnInit(): void {}
+
+//   cancel(): void {
+//     this.router.navigate(['/bulk-notifications']);
+//   }
+
+//   next(): void {
+//     // Implement the next step logic here
+//   }
+// }
+
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+
+// interface Channel {
+//   name: string;
+//   language?: string;
+//   messageHeader?: string;
+//   messageBody?: string;
+//   image?: File;
+// }
+
+// @Component({
+//   selector: 'app-create-bulk-notification',
+//   templateUrl: './create-bulk-notification.component.html',
+//   styleUrls: ['./create-bulk-notification.component.css']
+// })
+// export class CreateBulkNotificationComponent implements OnInit {
+//   channels = [
+//     { id: 1, name: 'In-App Notification' },
+//     { id: 2, name: 'SMS' },
+//     { id: 3, name: 'Email' }
+//   ];
+
+//   selectedChannels: Channel[] = [
+//     { name: 'In-App Notification' },
+//     { name: 'SMS' },
+//     { name: 'Email' }
+//   ];
+
+//   constructor(private router: Router) {}
+
+//   ngOnInit(): void {}
+
+//   cancel(): void {
+//     this.router.navigate(['/bulk-notifications']);
+//   }
+
+//   next(): void {
+//     // Implement the next step logic here
+//   }
+
+//   onFileChange(event: any, channel: Channel): void {
+//     const input = event.target as HTMLInputElement;
+//     if (input.files && input.files.length) {
+//       channel.image = input.files[0];
+//     }
+//   }
+// }
+
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-create-bulk-notification',
+//   templateUrl: './create-bulk-notification.component.html',
+//   styleUrls: ['./create-bulk-notification.component.css']
+// })
+// export class CreateBulkNotificationComponent implements OnInit {
+//   activeTab = 'create';
+//   channels = [
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false }
+//   ];
+//   filteredOptions = ['In-App Notification', 'SMS', 'Email'];
+
+//   constructor() { }
+
+//   ngOnInit(): void { }
+
+//   setActiveTab(tab: string): void {
+//     this.activeTab = tab;
+//   }
+
+//   showOptions(channel: any): void {
+//     channel.showOptions = true;
+//   }
+
+//   hideOptions(channel: any): void {
+//     setTimeout(() => channel.showOptions = false, 200); // Delay to allow click event to register
+//   }
+
+//   selectOption(channel: any, option: string): void {
+//     channel.name = option;
+//     channel.showOptions = false;
+//   }
+
+//   cancel(): void {
+//     // Implement cancel logic
+//   }
+
+//   next(): void {
+//     // Implement next logic
+//   }
+// }
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-bulk-notification',
@@ -7,21 +198,45 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-bulk-notification.component.css']
 })
 export class CreateBulkNotificationComponent implements OnInit {
+  activeTab = 'create';
   channels = [
-    { name: 'In-App Notification' },
-    { name: 'SMS' },
-    { name: 'Email' }
+    { name: '', language: '', header: '', body: '', image: null, showOptions: false }
   ];
+  filteredOptions = ['In-App Notification', 'SMS', 'Email'];
 
-  constructor(private router: Router) {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
+
+  setActiveTab(tab: string): void {
+    this.activeTab = tab;
+  }
+
+  showOptions(channel: any): void {
+    channel.showOptions = true;
+  }
+
+  hideOptions(channel: any): void {
+    setTimeout(() => channel.showOptions = false, 200); // Delay to allow click event to register
+  }
+
+  selectOption(channel: any, option: string): void {
+    channel.name = option;
+    channel.showOptions = false;
+  }
 
   cancel(): void {
-    this.router.navigate(['/bulk-notifications']);
+    // Implement cancel logic
   }
 
   next(): void {
-    // Implement the next step logic here
+    // Implement next logic
+  }
+
+  onFileChange(event: any, channel: any): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      channel.image = input.files[0];
+    }
   }
 }
