c7b6ae4826b2cc4ed1c1c6718839831552a14952
Sruti Dash <sruti.dash1203@gmail.com>
2024-07-22 12:20:14 +0530

    added the fucntionality of choose file in the image section of the create bulk notification layout

--- src/app/notification/create-bulk-notification/create-bulk-notification.component.ts
+++ src/app/notification/create-bulk-notification/create-bulk-notification.component.ts
@@ -190,7 +190,130 @@
 //     // Implement next logic
 //   }
 // }
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-create-bulk-notification',
+//   templateUrl: './create-bulk-notification.component.html',
+//   styleUrls: ['./create-bulk-notification.component.css']
+// })
+// export class CreateBulkNotificationComponent implements OnInit {
+//   activeTab = 'create';
+//   channels = [
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false }
+//   ];
+//   filteredOptions = ['In-App Notification', 'SMS', 'Email'];
+
+//   constructor() { }
+
+//   ngOnInit(): void { }
+
+//   setActiveTab(tab: string): void {
+//     this.activeTab = tab;
+//   }
+
+//   showOptions(channel: any): void {
+//     channel.showOptions = true;
+//   }
+
+//   hideOptions(channel: any): void {
+//     setTimeout(() => channel.showOptions = false, 200); // Delay to allow click event to register
+//   }
+
+//   selectOption(channel: any, option: string): void {
+//     channel.name = option;
+//     channel.showOptions = false;
+//   }
+
+//   cancel(): void {
+//     // Implement cancel logic
+//   }
+
+//   next(): void {
+//     // Implement next logic
+//   }
+
+//   onFileChange(event: any, channel: any): void {
+//     const input = event.target as HTMLInputElement;
+//     if (input.files && input.files.length > 0) {
+//       channel.image = input.files[0];
+//     }
+//   }
+// }
+
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-create-bulk-notification',
+//   templateUrl: './create-bulk-notification.component.html',
+//   styleUrls: ['./create-bulk-notification.component.css']
+// })
+// export class CreateBulkNotificationComponent implements OnInit {
+//   activeTab = 'create';
+//   channels = [
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [] }
+//   ];
+//   options = ['In-App Notification', 'SMS', 'Email'];
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//     this.channels.forEach(channel => {
+//       channel.filteredOptions = [...this.options];
+//     });
+//   }
+
+//   setActiveTab(tab: string): void {
+//     this.activeTab = tab;
+//   }
+
+//   showOptions(channel: any): void {
+//     channel.showOptions = true;
+//   }
+
+//   hideOptions(channel: any): void {
+//     setTimeout(() => channel.showOptions = false, 200); // Delay to allow click event to register
+//   }
+
+//   filterOptions(channel: any): void {
+//     const query = channel.name.toLowerCase();
+//     channel.filteredOptions = this.options.filter(option => option.toLowerCase().includes(query));
+//   }
+
+//   selectOption(channel: any, option: string): void {
+//     channel.name = option;
+//     channel.showOptions = false;
+//   }
+
+//   cancel(): void {
+//     // Implement cancel logic
+//   }
+
+//   next(): void {
+//     // Implement next logic
+//   }
+
+//   onFileChange(event: any, channel: any): void {
+//     const input = event.target as HTMLInputElement;
+//     if (input.files && input.files.length > 0) {
+//       channel.image = input.files[0];
+//     }
+//   }
+// }
+
 import { Component, OnInit } from '@angular/core';
+
+interface Channel {
+  name: string;
+  language: string;
+  header: string;
+  body: string;
+  image: File | null;
+  showOptions: boolean;
+  filteredOptions: string[];
+}
 
 @Component({
   selector: 'app-create-bulk-notification',
@@ -199,28 +322,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateBulkNotificationComponent implements OnInit {
   activeTab = 'create';
-  channels = [
-    { name: '', language: '', header: '', body: '', image: null, showOptions: false }
+  channels: Channel[] = [
+    { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [] },
+    { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [] },
+    { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [] }
   ];
-  filteredOptions = ['In-App Notification', 'SMS', 'Email'];
+  options: string[] = ['In-App Notification', 'SMS', 'Email'];
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.channels.forEach(channel => {
+      channel.filteredOptions = [...this.options];
+    });
+  }
 
   setActiveTab(tab: string): void {
     this.activeTab = tab;
   }
 
-  showOptions(channel: any): void {
+  showOptions(channel: Channel): void {
     channel.showOptions = true;
   }
 
-  hideOptions(channel: any): void {
+  hideOptions(channel: Channel): void {
     setTimeout(() => channel.showOptions = false, 200); // Delay to allow click event to register
   }
 
-  selectOption(channel: any, option: string): void {
+  filterOptions(channel: Channel): void {
+    const query = channel.name.toLowerCase();
+    channel.filteredOptions = this.options.filter(option => option.toLowerCase().includes(query));
+  }
+
+  selectOption(channel: Channel, option: string): void {
     channel.name = option;
     channel.showOptions = false;
   }
@@ -233,7 +367,7 @@ export class CreateBulkNotificationComponent implements OnInit {
     // Implement next logic
   }
 
-  onFileChange(event: any, channel: any): void {
+  onFileChange(event: any, channel: Channel): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       channel.image = input.files[0];
