d251a37ce13213ef15548de36dbaa737342610e0
Sruti Dash <sruti.dash1203@gmail.com>
2024-07-23 18:59:58 +0530

    added channel, language searchable menus and image columns functionalitires

--- src/app/notification/create-bulk-notification/create-bulk-notification.component.ts
+++ src/app/notification/create-bulk-notification/create-bulk-notification.component.ts
@@ -1,3 +1,316 @@
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+
+// interface Channel {
+//   name: string;
+//   language: string;
+//   header: string;
+//   body: string;
+//   image: File | null;
+//   showOptions: boolean;
+//   filteredOptions: string[];
+//   showLanguageOptions: boolean;
+//   filteredLanguageOptions: string[];
+// }
+
+// @Component({
+//   selector: 'app-create-bulk-notification',
+//   templateUrl: './create-bulk-notification.component.html',
+//   styleUrls: ['./create-bulk-notification.component.css']
+// })
+// export class CreateBulkNotificationComponent implements OnInit {
+//   activeTab = 'create';
+//   channels: Channel[] = [
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [], showLanguageOptions: false, filteredLanguageOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [], showLanguageOptions: false, filteredLanguageOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [], showLanguageOptions: false, filteredLanguageOptions: [] }
+//   ];
+//   options: string[] = ['In-App Notification', 'SMS', 'Email'];
+//   languageOptions: string[] = ['English'];
+//   showFileDialog = false;
+//   selectedChannel: Channel | null = null;
+//   isNextEnabled = false;
+
+//   constructor(private router: Router) { }
+
+//   ngOnInit(): void {
+//     this.channels.forEach(channel => {
+//       channel.filteredOptions = [...this.options];
+//       channel.filteredLanguageOptions = [...this.languageOptions];
+//     });
+//   }
+
+//   setActiveTab(tab: string): void {
+//     this.activeTab = tab;
+//   }
+
+//   showOptions(channel: Channel): void {
+//     channel.showOptions = true;
+//   }
+
+//   hideOptions(channel: Channel): void {
+//     setTimeout(() => channel.showOptions = false, 200); // Delay to allow click event to register
+//   }
+
+//   filterOptions(channel: Channel): void {
+//     const query = channel.name.toLowerCase();
+//     channel.filteredOptions = this.options.filter(option => option.toLowerCase().includes(query));
+//   }
+
+//   selectOption(channel: Channel, option: string): void {
+//     channel.name = option;
+//     channel.showOptions = false;
+//   }
+
+//   showLanguageOptions(channel: Channel): void {
+//     channel.showLanguageOptions = true;
+//   }
+
+//   hideLanguageOptions(channel: Channel): void {
+//     setTimeout(() => channel.showLanguageOptions = false, 200); // Delay to allow click event to register
+//   }
+
+//   filterLanguageOptions(channel: Channel): void {
+//     const query = channel.language.toLowerCase();
+//     channel.filteredLanguageOptions = this.languageOptions.filter(option => option.toLowerCase().includes(query));
+//   }
+
+//   selectLanguageOption(channel: Channel, option: string): void {
+//     channel.language = option;
+//     channel.showLanguageOptions = false;
+//   }
+
+//   openFileDialog(channel: Channel): void {
+//     this.selectedChannel = channel;
+//     this.showFileDialog = true;
+//   }
+
+//   onFileSelected(file: File | null, channel: Channel): void {
+//     if (file) {
+//       channel.image = file;
+//       this.isNextEnabled = true;
+//     }
+//     this.showFileDialog = false;
+//   }
+
+//   cancel(): void {
+//     // Implement cancel logic
+//   }
+
+//   next(): void {
+//     // Navigate to schedule page
+//     this.router.navigate(['/schedule']);
+//   }
+// }
+
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+
+// interface Channel {
+//   name: string;
+//   language: string;
+//   header: string;
+//   body: string;
+//   image: File | null;
+//   showOptions: boolean;
+//   showLanguageOptions: boolean;
+//   filteredOptions: string[];
+//   filteredLanguageOptions: string[];
+// }
+
+// @Component({
+//   selector: 'app-create-bulk-notification',
+//   templateUrl: './create-bulk-notification.component.html',
+//   styleUrls: ['./create-bulk-notification.component.css']
+// })
+// export class CreateBulkNotificationComponent implements OnInit {
+//   activeTab = 'create';
+//   channels: Channel[] = [
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, showLanguageOptions: false, filteredOptions: [], filteredLanguageOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, showLanguageOptions: false, filteredOptions: [], filteredLanguageOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, showLanguageOptions: false, filteredOptions: [], filteredLanguageOptions: [] }
+//   ];
+//   options: string[] = ['In-App Notification', 'SMS', 'Email'];
+//   languageOptions: string[] = ['English', 'Spanish', 'French'];
+//   showFileDialog = false;
+//   selectedChannel: Channel | null = null;
+
+//   constructor(private router: Router) { }
+
+//   ngOnInit(): void {
+//     this.channels.forEach(channel => {
+//       channel.filteredOptions = [...this.options];
+//       channel.filteredLanguageOptions = [...this.languageOptions];
+//     });
+//   }
+
+//   setActiveTab(tab: string): void {
+//     this.activeTab = tab;
+//   }
+
+//   showOptions(channel: Channel): void {
+//     channel.showOptions = true;
+//   }
+
+//   hideOptions(channel: Channel): void {
+//     setTimeout(() => channel.showOptions = false, 200); // Delay to allow click event to register
+//   }
+
+//   filterOptions(channel: Channel): void {
+//     const query = channel.name.toLowerCase();
+//     channel.filteredOptions = this.options.filter(option => option.toLowerCase().includes(query));
+//   }
+
+//   selectOption(channel: Channel, option: string): void {
+//     channel.name = option;
+//     channel.showOptions = false;
+//   }
+
+//   showLanguageOptions(channel: Channel): void {
+//     channel.showLanguageOptions = true;
+//   }
+
+//   hideLanguageOptions(channel: Channel): void {
+//     setTimeout(() => channel.showLanguageOptions = false, 200); // Delay to allow click event to register
+//   }
+
+//   filterLanguageOptions(channel: Channel): void {
+//     const query = channel.language.toLowerCase();
+//     channel.filteredLanguageOptions = this.languageOptions.filter(option => option.toLowerCase().includes(query));
+//   }
+
+//   selectLanguageOption(channel: Channel, option: string): void {
+//     channel.language = option;
+//     channel.showLanguageOptions = false;
+//   }
+
+//   openFileDialog(channel: Channel): void {
+//     this.selectedChannel = channel;
+//     this.showFileDialog = true;
+//   }
+
+//   onFileSelected(event: File, channel: Channel): void {
+//     if (channel) {
+//       channel.image = event;
+//       this.showFileDialog = false;
+//     }
+//   }
+
+//   cancel(): void {
+//     // Implement cancel logic
+//   }
+
+//   next(): void {
+//     // Implement next logic
+//     this.router.navigate(['/schedule']);
+//   }
+// }
+
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+
+// interface Channel {
+//   name: string;
+//   language: string;
+//   header: string;
+//   body: string;
+//   image: File | null;
+//   showOptions: boolean;
+//   filteredOptions: string[];
+//   showLanguageOptions: boolean;
+//   filteredLanguageOptions: string[];
+// }
+
+// @Component({
+//   selector: 'app-create-bulk-notification',
+//   templateUrl: './create-bulk-notification.component.html',
+//   styleUrls: ['./create-bulk-notification.component.css']
+// })
+// export class CreateBulkNotificationComponent implements OnInit {
+//   activeTab = 'create';
+//   channels: Channel[] = [
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [], showLanguageOptions: false, filteredLanguageOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [], showLanguageOptions: false, filteredLanguageOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [], showLanguageOptions: false, filteredLanguageOptions: [] }
+//   ];
+//   options: string[] = ['In-App Notification', 'SMS', 'Email'];
+//   languageOptions: string[] = ['English'];
+//   showFileDialog = false;
+//   selectedChannel: Channel | null = null;
+//   isNextEnabled = false;
+
+//   constructor(private router: Router) { }
+
+//   ngOnInit(): void {
+//     this.channels.forEach(channel => {
+//       channel.filteredOptions = [...this.options];
+//       channel.filteredLanguageOptions = [...this.languageOptions];
+//     });
+//   }
+
+//   setActiveTab(tab: string): void {
+//     this.activeTab = tab;
+//   }
+
+//   showOptions(channel: Channel): void {
+//     channel.showOptions = true;
+//   }
+
+//   hideOptions(channel: Channel): void {
+//     setTimeout(() => channel.showOptions = false, 200); // Delay to allow click event to register
+//   }
+
+//   filterOptions(channel: Channel): void {
+//     const query = channel.name.toLowerCase();
+//     channel.filteredOptions = this.options.filter(option => option.toLowerCase().includes(query));
+//   }
+
+//   selectOption(channel: Channel, option: string): void {
+//     channel.name = option;
+//     channel.showOptions = false;
+//   }
+
+//   showLanguageOptions(channel: Channel): void {
+//     channel.showLanguageOptions = true;
+//   }
+
+//   hideLanguageOptions(channel: Channel): void {
+//     setTimeout(() => channel.showLanguageOptions = false, 200); // Delay to allow click event to register
+//   }
+
+//   filterLanguageOptions(channel: Channel): void {
+//     const query = channel.language.toLowerCase();
+//     channel.filteredLanguageOptions = this.languageOptions.filter(option => option.toLowerCase().includes(query));
+//   }
+
+//   selectLanguageOption(channel: Channel, option: string): void {
+//     channel.language = option;
+//     channel.showLanguageOptions = false;
+//   }
+
+//   openFileDialog(channel: Channel): void {
+//     this.selectedChannel = channel;
+//     this.showFileDialog = true;
+//   }
+
+//   onFileSelected(file: File | null, channel: Channel): void {
+//     if (file) {
+//       channel.image = file;
+//       this.isNextEnabled = true;
+//     }
+//     this.showFileDialog = false;
+//   }
+
+//   cancel(): void {
+//     // Implement cancel logic
+//   }
+
+//   next(): void {
+//     // Navigate to schedule page
+//     this.router.navigate(['/schedule']);
+//   }
+// }
+
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -98,7 +411,8 @@ export class CreateBulkNotificationComponent implements OnInit {
   }
 
   next(): void {
-    // Navigate to schedule page
-    this.router.navigate(['/schedule']);
+    if (this.isNextEnabled) {
+      this.router.navigate(['/schedule']);
+    }
   }
 }
