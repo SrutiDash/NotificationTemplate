9fe79693f81505e65997a4648d0859e719aa2de2
Sruti Dash <sruti.dash1203@gmail.com>
2024-07-23 08:31:05 +0530

    added dialog box for file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,183 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
+// import { NgSelectModule } from '@ng-select/ng-select';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { FilterPromptComponent } from './notification/filter-prompt/filter-prompt.component';
+// import { CreateNotificationComponent } from './notification/create-notification/create-notification.component';
+// import { NotificationManagementComponent } from './notification/notification-management/notification-management.component';
+// import { NotificationEditComponent } from './notification/notification-edit/notification-edit.component';
+// import { ReviewConfirmComponent } from './notification/review-confirm/review-confirm.component';
+// import { BulkNotificationsComponent } from './notification/bulk-notifications/bulk-notifications.component';
+// import { FilterPipe } from './filter.pipe';
+// import { CreateBulkNotificationComponent } from './notification/create-bulk-notification/create-bulk-notification.component';
+// import { FileDialogComponent } from './notification/file-dialog/file-dialog.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     FilterPromptComponent,
+//     CreateNotificationComponent,
+//     NotificationManagementComponent,
+//     NotificationEditComponent,
+//     ReviewConfirmComponent,
+//     BulkNotificationsComponent,
+//     FilterPipe,
+//     CreateBulkNotificationComponent,
+//     FileDialogComponent,
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     FormsModule,
+//     NgSelectModule,
+//     HttpClientModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
+// import { NgSelectModule } from '@ng-select/ng-select';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { FilterPromptComponent } from './notification/filter-prompt/filter-prompt.component';
+// import { CreateNotificationComponent } from './notification/create-notification/create-notification.component';
+// import { NotificationManagementComponent } from './notification/notification-management/notification-management.component';
+// import { NotificationEditComponent } from './notification/notification-edit/notification-edit.component';
+// import { ReviewConfirmComponent } from './notification/review-confirm/review-confirm.component';
+// import { BulkNotificationsComponent } from './notification/bulk-notifications/bulk-notifications.component';
+// import { FilterPipe } from './filter.pipe';
+// import { CreateBulkNotificationComponent } from './notification/create-bulk-notification/create-bulk-notification.component';
+// import { FileDialogComponent } from './notification/file-dialog/file-dialog.component';
+
+// import { MatDialogModule } from '@angular/material/dialog';
+// import { MatButtonModule } from '@angular/material/button';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     FilterPromptComponent,
+//     CreateNotificationComponent,
+//     NotificationManagementComponent,
+//     NotificationEditComponent,
+//     ReviewConfirmComponent,
+//     BulkNotificationsComponent,
+//     FilterPipe,
+//     CreateBulkNotificationComponent,
+//     FileDialogComponent,
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     FormsModule,
+//     NgSelectModule,
+//     HttpClientModule,
+//     MatDialogModule,
+//     MatButtonModule,
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent],
+//   entryComponents: [FileDialogComponent] // Add this line
+// })
+// export class AppModule { }
+
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
+// import { NgSelectModule } from '@ng-select/ng-select';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { FilterPromptComponent } from './notification/filter-prompt/filter-prompt.component';
+// import { CreateNotificationComponent } from './notification/create-notification/create-notification.component';
+// import { NotificationManagementComponent } from './notification/notification-management/notification-management.component';
+// import { NotificationEditComponent } from './notification/notification-edit/notification-edit.component';
+// import { ReviewConfirmComponent } from './notification/review-confirm/review-confirm.component';
+// import { BulkNotificationsComponent } from './notification/bulk-notifications/bulk-notifications.component';
+// import { FilterPipe } from './filter.pipe';
+// import { CreateBulkNotificationComponent } from './notification/create-bulk-notification/create-bulk-notification.component';
+// import { FileDialogComponent } from './notification/file-dialog/file-dialog.component';
+
+// import { MatDialogModule } from '@angular/material/dialog';
+// import { MatButtonModule } from '@angular/material/button';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     FilterPromptComponent,
+//     CreateNotificationComponent,
+//     NotificationManagementComponent,
+//     NotificationEditComponent,
+//     ReviewConfirmComponent,
+//     BulkNotificationsComponent,
+//     FilterPipe,
+//     CreateBulkNotificationComponent,
+//     FileDialogComponent,
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     FormsModule,
+//     NgSelectModule,
+//     HttpClientModule,
+//     MatDialogModule,
+//     MatButtonModule,
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent],
+//   entryComponents: [FileDialogComponent] // Add this line
+// })
+// export class AppModule { }
+
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
+// import { NgSelectModule } from '@ng-select/ng-select';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { FilterPromptComponent } from './notification/filter-prompt/filter-prompt.component';
+// import { CreateNotificationComponent } from './notification/create-notification/create-notification.component';
+// import { NotificationManagementComponent } from './notification/notification-management/notification-management.component';
+// import { NotificationEditComponent } from './notification/notification-edit/notification-edit.component';
+// import { ReviewConfirmComponent } from './notification/review-confirm/review-confirm.component';
+// import { BulkNotificationsComponent } from './notification/bulk-notifications/bulk-notifications.component';
+// import { FilterPipe } from './filter.pipe';
+// import { CreateBulkNotificationComponent } from './notification/create-bulk-notification/create-bulk-notification.component';
+// import { FileDialogComponent } from './notification/file-dialog/file-dialog.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     FilterPromptComponent,
+//     CreateNotificationComponent,
+//     NotificationManagementComponent,
+//     NotificationEditComponent,
+//     ReviewConfirmComponent,
+//     BulkNotificationsComponent,
+//     FilterPipe,
+//     CreateBulkNotificationComponent,
+//     FileDialogComponent,
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     FormsModule,
+//     NgSelectModule,
+//     HttpClientModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -13,6 +193,7 @@ import { ReviewConfirmComponent } from './notification/review-confirm/review-con
 import { BulkNotificationsComponent } from './notification/bulk-notifications/bulk-notifications.component';
 import { FilterPipe } from './filter.pipe';
 import { CreateBulkNotificationComponent } from './notification/create-bulk-notification/create-bulk-notification.component';
+import { FileDialogComponent } from './notification/file-dialog/file-dialog.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +206,7 @@ import { CreateBulkNotificationComponent } from './notification/create-bulk-noti
     BulkNotificationsComponent,
     FilterPipe,
     CreateBulkNotificationComponent,
+    FileDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,4 +219,3 @@ import { CreateBulkNotificationComponent } from './notification/create-bulk-noti
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
--- src/app/notification/create-bulk-notification/create-bulk-notification.component.ts
+++ src/app/notification/create-bulk-notification/create-bulk-notification.component.ts
@@ -70,6 +70,351 @@
 //   }
 // }
 
+// import { Component, OnInit } from '@angular/core';
+
+// interface Channel {
+//   name: string;
+//   language: string;
+//   header: string;
+//   body: string;
+//   image: File | null;
+//   showOptions: boolean;
+//   showLanguageOptions: boolean;
+//   filteredOptions: string[];
+//   filteredLanguageOptions: string[];
+// }
+
+// @Component({
+//   selector: 'app-create-bulk-notification',
+//   templateUrl: './create-bulk-notification.component.html',
+//   styleUrls: ['./create-bulk-notification.component.css']
+// })
+// export class CreateBulkNotificationComponent implements OnInit {
+//   activeTab = 'create';
+//   channels: Channel[] = [
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, showLanguageOptions: false, filteredOptions: [], filteredLanguageOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, showLanguageOptions: false, filteredOptions: [], filteredLanguageOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, showLanguageOptions: false, filteredOptions: [], filteredLanguageOptions: [] }
+//   ];
+//   options: string[] = ['In-App Notification', 'SMS', 'Email'];
+//   languageOptions: string[] = ['English'];
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//     this.channels.forEach(channel => {
+//       channel.filteredOptions = [...this.options];
+//       channel.filteredLanguageOptions = [...this.languageOptions];
+//     });
+//   }
+
+//   setActiveTab(tab: string): void {
+//     this.activeTab = tab;
+//   }
+
+//   showOptions(channel: Channel): void {
+//     channel.showOptions = true;
+//   }
+
+//   hideOptions(channel: Channel): void {
+//     setTimeout(() => channel.showOptions = false, 200); // Delay to allow click event to register
+//   }
+
+//   filterOptions(channel: Channel): void {
+//     const query = channel.name.toLowerCase();
+//     channel.filteredOptions = this.options.filter(option => option.toLowerCase().includes(query));
+//   }
+
+//   selectOption(channel: Channel, option: string): void {
+//     channel.name = option;
+//     channel.showOptions = false;
+//   }
+
+//   showLanguageOptions(channel: Channel): void {
+//     channel.showLanguageOptions = true;
+//   }
+
+//   hideLanguageOptions(channel: Channel): void {
+//     setTimeout(() => channel.showLanguageOptions = false, 200); // Delay to allow click event to register
+//   }
+
+//   filterLanguageOptions(channel: Channel): void {
+//     const query = channel.language.toLowerCase();
+//     channel.filteredLanguageOptions = this.languageOptions.filter(option => option.toLowerCase().includes(query));
+//   }
+
+//   selectLanguageOption(channel: Channel, option: string): void {
+//     channel.language = option;
+//     channel.showLanguageOptions = false;
+//   }
+
+//   cancel(): void {
+//     // Implement cancel logic
+//   }
+
+//   next(): void {
+//     // Implement next logic
+//   }
+
+//   onFileChange(event: any, channel: Channel): void {
+//     const input = event.target as HTMLInputElement;
+//     if (input.files && input.files.length > 0) {
+//       channel.image = input.files[0];
+//     }
+//   }
+// }
+
+// import { Component, OnInit } from '@angular/core';
+// import { MatDialog } from '@angular/material/dialog';
+// import { FileDialogComponent } from '../file-dialog/file-dialog.component';
+
+// interface Channel {
+//   name: string;
+//   language: string;
+//   header: string;
+//   body: string;
+//   image: File | null;
+//   showOptions: boolean;
+//   filteredOptions: string[];
+// }
+
+// @Component({
+//   selector: 'app-create-bulk-notification',
+//   templateUrl: './create-bulk-notification.component.html',
+//   styleUrls: ['./create-bulk-notification.component.css']
+// })
+// export class CreateBulkNotificationComponent implements OnInit {
+//   activeTab = 'create';
+//   channels: Channel[] = [
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [] }
+//   ];
+//   options: string[] = ['In-App Notification', 'SMS', 'Email'];
+
+//   constructor(public dialog: MatDialog) {}
+
+//   ngOnInit(): void {
+//     this.channels.forEach(channel => {
+//       channel.filteredOptions = [...this.options];
+//     });
+//   }
+
+//   setActiveTab(tab: string): void {
+//     this.activeTab = tab;
+//   }
+
+//   showOptions(channel: Channel): void {
+//     channel.showOptions = true;
+//   }
+
+//   hideOptions(channel: Channel): void {
+//     setTimeout(() => channel.showOptions = false, 200); // Delay to allow click event to register
+//   }
+
+//   filterOptions(channel: Channel): void {
+//     const query = channel.name.toLowerCase();
+//     channel.filteredOptions = this.options.filter(option => option.toLowerCase().includes(query));
+//   }
+
+//   selectOption(channel: Channel, option: string): void {
+//     channel.name = option;
+//     channel.showOptions = false;
+//   }
+
+//   cancel(): void {
+//     // Implement cancel logic
+//   }
+
+//   next(): void {
+//     // Implement next logic
+//   }
+
+//   onFileChange(channel: Channel): void {
+//     const dialogRef = this.dialog.open(FileDialogComponent);
+
+//     dialogRef.afterClosed().subscribe(result => {
+//       if (result) {
+//         channel.image = result;
+//       }
+//     });
+//   }
+// }
+
+// import { Component, OnInit } from '@angular/core';
+
+// interface Channel {
+//   name: string;
+//   language: string;
+//   header: string;
+//   body: string;
+//   image: File | null;
+//   showOptions: boolean;
+//   filteredOptions: string[];
+//   showLanguageOptions: boolean;
+//   filteredLanguageOptions: string[];
+// }
+
+// @Component({
+//   selector: 'app-create-bulk-notification',
+//   templateUrl: './create-bulk-notification.component.html',
+//   styleUrls: ['./create-bulk-notification.component.css']
+// })
+// export class CreateBulkNotificationComponent implements OnInit {
+//   activeTab = 'create';
+//   channels: Channel[] = [
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [], showLanguageOptions: false, filteredLanguageOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [], showLanguageOptions: false, filteredLanguageOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [], showLanguageOptions: false, filteredLanguageOptions: [] }
+//   ];
+//   options: string[] = ['In-App Notification', 'SMS', 'Email'];
+//   languageOptions: string[] = ['English'];
+//   showFileDialog: boolean = false;
+//   selectedChannel: Channel | null = null;
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//     this.channels.forEach(channel => {
+//       channel.filteredOptions = [...this.options];
+//       channel.filteredLanguageOptions = [...this.languageOptions];
+//     });
+//   }
+
+//   setActiveTab(tab: string): void {
+//     this.activeTab = tab;
+//   }
+
+//   showOptions(channel: Channel): void {
+//     channel.showOptions = true;
+//   }
+
+//   hideOptions(channel: Channel): void {
+//     setTimeout(() => channel.showOptions = false, 200); // Delay to allow click event to register
+//   }
+
+//   filterOptions(channel: Channel): void {
+//     const query = channel.name.toLowerCase();
+//     channel.filteredOptions = this.options.filter(option => option.toLowerCase().includes(query));
+//   }
+
+//   selectOption(channel: Channel, option: string): void {
+//     channel.name = option;
+//     channel.showOptions = false;
+//   }
+
+//   showLanguageOptions(channel: Channel): void {
+//     channel.showLanguageOptions = true;
+//   }
+
+//   hideLanguageOptions(channel: Channel): void {
+//     setTimeout(() => channel.showLanguageOptions = false, 200); // Delay to allow click event to register
+//   }
+
+//   filterLanguageOptions(channel: Channel): void {
+//     const query = channel.language.toLowerCase();
+//     channel.filteredLanguageOptions = this.languageOptions.filter(option => option.toLowerCase().includes(query));
+//   }
+
+//   selectLanguageOption(channel: Channel, option: string): void {
+//     channel.language = option;
+//     channel.showLanguageOptions = false;
+//   }
+
+//   openFileDialog(channel: Channel): void {
+//     this.selectedChannel = channel;
+//     this.showFileDialog = true;
+//   }
+
+//   onFileSelected(file: File, channel: Channel | null): void {
+//     if (channel) {
+//       channel.image = file;
+//     }
+//     this.showFileDialog = false;
+//   }
+
+//   cancel(): void {
+//     // Implement cancel logic
+//   }
+
+//   next(): void {
+//     // Implement next logic
+//   }
+// }
+
+// import { Component, OnInit } from '@angular/core';
+
+// interface Channel {
+//   name: string;
+//   language: string;
+//   header: string;
+//   body: string;
+//   image: File | null;
+//   showOptions: boolean;
+//   filteredOptions: string[];
+// }
+
+// @Component({
+//   selector: 'app-create-bulk-notification',
+//   templateUrl: './create-bulk-notification.component.html',
+//   styleUrls: ['./create-bulk-notification.component.css']
+// })
+// export class CreateBulkNotificationComponent implements OnInit {
+//   activeTab = 'create';
+//   channels: Channel[] = [
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [] },
+//     { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [] }
+//   ];
+//   options: string[] = ['In-App Notification', 'SMS', 'Email'];
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//     this.channels.forEach(channel => {
+//       channel.filteredOptions = [...this.options];
+//     });
+//   }
+
+//   setActiveTab(tab: string): void {
+//     this.activeTab = tab;
+//   }
+
+//   showOptions(channel: Channel): void {
+//     channel.showOptions = true;
+//   }
+
+//   hideOptions(channel: Channel): void {
+//     setTimeout(() => channel.showOptions = false, 200); // Delay to allow click event to register
+//   }
+
+//   filterOptions(channel: Channel): void {
+//     const query = channel.name.toLowerCase();
+//     channel.filteredOptions = this.options.filter(option => option.toLowerCase().includes(query));
+//   }
+
+//   selectOption(channel: Channel, option: string): void {
+//     channel.name = option;
+//     channel.showOptions = false;
+//   }
+
+//   cancel(): void {
+//     // Implement cancel logic
+//   }
+
+//   next(): void {
+//     // Implement next logic
+//   }
+
+//   onFileChange(event: any, channel: Channel): void {
+//     const input = event.target as HTMLInputElement;
+//     if (input.files && input.files.length > 0) {
+//       channel.image = input.files[0];
+//       input.value = ''; // Reset the input value to allow the same file to be selected again
+//     }
+//   }
+// }
+
 import { Component, OnInit } from '@angular/core';
 
 interface Channel {
@@ -79,8 +424,8 @@ interface Channel {
   body: string;
   image: File | null;
   showOptions: boolean;
-  showLanguageOptions: boolean;
   filteredOptions: string[];
+  showLanguageOptions: boolean;
   filteredLanguageOptions: string[];
 }
 
@@ -92,12 +437,15 @@ interface Channel {
 export class CreateBulkNotificationComponent implements OnInit {
   activeTab = 'create';
   channels: Channel[] = [
-    { name: '', language: '', header: '', body: '', image: null, showOptions: false, showLanguageOptions: false, filteredOptions: [], filteredLanguageOptions: [] },
-    { name: '', language: '', header: '', body: '', image: null, showOptions: false, showLanguageOptions: false, filteredOptions: [], filteredLanguageOptions: [] },
-    { name: '', language: '', header: '', body: '', image: null, showOptions: false, showLanguageOptions: false, filteredOptions: [], filteredLanguageOptions: [] }
+    { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [], showLanguageOptions: false, filteredLanguageOptions: [] },
+    { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [], showLanguageOptions: false, filteredLanguageOptions: [] },
+    { name: '', language: '', header: '', body: '', image: null, showOptions: false, filteredOptions: [], showLanguageOptions: false, filteredLanguageOptions: [] }
   ];
   options: string[] = ['In-App Notification', 'SMS', 'Email'];
   languageOptions: string[] = ['English'];
+
+  selectedChannel: Channel | null = null;
+  showFileDialog = false;
 
   constructor() { }
 
@@ -156,10 +504,16 @@ export class CreateBulkNotificationComponent implements OnInit {
     // Implement next logic
   }
 
-  onFileChange(event: any, channel: Channel): void {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files.length > 0) {
-      channel.image = input.files[0];
+  openFileDialog(channel: Channel): void {
+    this.selectedChannel = channel;
+    this.showFileDialog = true;
+  }
+
+  onFileSelected(file: File | null): void {
+    if (this.selectedChannel && file) {
+      this.selectedChannel.image = file;
     }
+    this.showFileDialog = false;
+    this.selectedChannel = null;
   }
 }
