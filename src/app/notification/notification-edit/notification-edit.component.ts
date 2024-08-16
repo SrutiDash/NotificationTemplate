380459f1d6b41949694b25810ff595df47f1a326
Sruti Dash <sruti.dash1203@gmail.com>
2024-08-16 12:54:42 +0530

    added  language and text over edit layout

--- src/app/notification/notification-edit/notification-edit.component.ts
+++ src/app/notification/notification-edit/notification-edit.component.ts
@@ -9,82 +9,8 @@
 // })
 // export class NotificationEditComponent implements OnInit {
 //   notification: any = {};
-//   search: string = '';
-
-//   constructor(private route: ActivatedRoute, private router: Router, private dataService: DataService) { }
-
-//   ngOnInit(): void {
-//     const notificationId = this.route.snapshot.paramMap.get('id');
-//     // Fetch the notification details based on the ID
-//     // This is sample data, replace it with actual data fetching logic
-//     this.notification = {
-//       serviceType: 'Send Money',
-//       eventTrigger: 'Success',
-//       party: 'Sender',
-//       senderParent: 'Customer',
-//       grade: 'Gold',
-//       createdBy: 'Chnladmin',
-//       createdOn: '12/07/2020 11:00hrs',
-//       status: 'Approved',
-//       inAppNotification: {
-//         language: 'English',
-//         header: 'Payment Successful',
-//         messageBody: 'Dear <First Name> <Last Name>, <Transaction Amount> is debited from <Currency> <Wallet> with <Transaction ID>.',
-//         attachableDocuments: 'Statement'
-//       },
-//       sms: {
-//         language: 'English',
-//         header: '-',
-//         body: 'We have just introduced multi currency wallets like United States Dollar and more. To activate multicurrency wallet, click here https://www.google.com/search?q=dummy+',
-//         attachableDocuments: 'Statement'
-//       },
-//       email: {
-//         language: 'English',
-//         header: 'Payment Successful',
-//         body: 'To activate multicurrency wallet, please click here https://www.google.com/search?q=dummy+ or call customer support',
-//         attachableDocuments: 'Statement'
-//       },
-//       url: {
-//         language: 'English',
-//         header: 'Payment Successful',
-//         body: 'To activate multicurrency wallet, please click here https://www.google.com/search?q=dummy+ or call customer support',
-//         attachableDocuments: 'Statement'
-//       }
-//     };
-//   }
-
-//   cancel(): void {
-//     this.router.navigate(['/notification-management']);
-//   }
-
-//   next(): void {
-//     this.dataService.selectedServiceType = this.notification.serviceType;
-//     this.dataService.selectedEventTrigger = this.notification.eventTrigger;
-//     this.dataService.selectedParty = this.notification.party;
-//     this.dataService.senderParent = this.notification.senderParent;
-//     this.dataService.grade = this.notification.grade;
-//     this.dataService.inAppNotifications = [this.notification.inAppNotification];
-//     this.dataService.smsNotifications = [this.notification.sms];
-//     this.dataService.emailNotifications = [this.notification.email];
-//     this.router.navigate(['/review-confirm']);
-//   }
-// }
-
-
-//new
-
-// import { Component, OnInit } from '@angular/core';
-// import { ActivatedRoute, Router } from '@angular/router';
-// import { DataService } from '../../services/data.service';
-
-// @Component({
-//   selector: 'app-notification-edit',
-//   templateUrl: './notification-edit.component.html',
-//   styleUrls: ['./notification-edit.component.css']
-// })
-// export class NotificationEditComponent implements OnInit {
-//   notification: any = {};
 //   languagesTexts: any[] = [];
+//   search: string = '';  // Add the search property here
 
 //   constructor(
 //     private route: ActivatedRoute,
@@ -123,6 +49,7 @@
 //   }
 // }
 
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from '../../services/data.service';
@@ -135,7 +62,6 @@ import { DataService } from '../../services/data.service';
 export class NotificationEditComponent implements OnInit {
   notification: any = {};
   languagesTexts: any[] = [];
-  search: string = '';  // Add the search property here
 
   constructor(
     private route: ActivatedRoute,
@@ -161,15 +87,9 @@ export class NotificationEditComponent implements OnInit {
   }
 
   next(): void {
-    // Assuming that you want to keep the existing data structure for saving
     this.dataService.selectedServiceType = this.notification.serviceType;
     this.dataService.selectedEventTrigger = this.notification.eventTrigger;
     this.dataService.selectedParty = this.notification.party;
-    this.dataService.senderParent = this.notification.senderParent;
-    this.dataService.grade = this.notification.grade;
-    this.dataService.inAppNotifications = [this.notification.inAppNotification];
-    this.dataService.smsNotifications = [this.notification.sms];
-    this.dataService.emailNotifications = [this.notification.email];
     this.router.navigate(['/review-confirm']);
   }
 }
