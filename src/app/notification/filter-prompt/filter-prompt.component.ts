8e1e7360f80319d1f267c17070b0ac969bb605f5
Sruti Dash <sruti.dash1203@gmail.com>
2024-08-21 14:32:51 +0530

    added searchable dropdown over filter

--- src/app/notification/filter-prompt/filter-prompt.component.ts
+++ src/app/notification/filter-prompt/filter-prompt.component.ts
@@ -1,178 +1,3 @@
-// // final
-
-// import { Component, EventEmitter, Output, OnInit } from '@angular/core';
-// import { DataService } from '../../services/data.service';
-
-// @Component({
-//   selector: 'app-filter-prompt',
-//   templateUrl: './filter-prompt.component.html',
-//   styleUrls: ['./filter-prompt.component.css']
-// })
-// export class FilterPromptComponent implements OnInit {
-//   @Output() close = new EventEmitter<void>();
-//   @Output() applyFilters = new EventEmitter<void>();
-
-//   serviceTypes: string[] = [];
-//   eventTriggers: string[] = [];
-//   parties: string[] = [];
-
-//   selectedServiceType: string = '';
-//   selectedEventTrigger: string = '';
-//   selectedParty: string = '';
-
-//   startDate: string = '';
-//   endDate: string = '';
-
-//   constructor(private dataService: DataService) {}
-
-//   ngOnInit() {
-//     this.dataService.getFilteredServiceTypes().subscribe(data => {
-//       console.log('Service Types:', data);  // Debug log
-//       this.serviceTypes = data;
-//     });
-//   }
-
-//   onServiceTypeChange() {
-//     console.log('Selected Service Type:', this.selectedServiceType);  // Debug log
-//     if (this.selectedServiceType) {
-//       this.dataService.getFilteredEventTriggers(this.selectedServiceType).subscribe(data => {
-//         console.log('Event Triggers:', data);  // Debug log
-//         this.eventTriggers = data;
-//         this.selectedEventTrigger = '';
-//         this.parties = [];
-//         this.selectedParty = '';
-//       });
-//     } else {
-//       this.eventTriggers = [];
-//       this.parties = [];
-//       this.selectedEventTrigger = '';
-//       this.selectedParty = '';
-//     }
-//   }
-
-//   onEventTriggerChange() {
-//     console.log('Selected Event Trigger:', this.selectedEventTrigger);  // Debug log
-//     if (this.selectedEventTrigger) {
-//       this.dataService.getFilteredParties(this.selectedServiceType, this.selectedEventTrigger).subscribe(data => {
-//         console.log('Parties:', data);  // Debug log
-//         this.parties = data;
-//         this.selectedParty = '';
-//       });
-//     } else {
-//       this.parties = [];
-//       this.selectedParty = '';
-//     }
-//   }
-
-//   applyFilter() {
-//     // Logic to apply filters
-//     this.applyFilters.emit();
-//     this.close.emit();
-//   }
-
-//   clearFilters() {
-//     this.selectedServiceType = '';
-//     this.selectedEventTrigger = '';
-//     this.selectedParty = '';
-//     this.startDate = '';
-//     this.endDate = '';
-//     this.eventTriggers = [];
-//     this.parties = [];
-//   }
-// }
-
-//newwww
-
-// import { Component, EventEmitter, Output, OnInit } from '@angular/core';
-// import { DataService } from '../../services/data.service';
-
-// @Component({
-//   selector: 'app-filter-prompt',
-//   templateUrl: './filter-prompt.component.html',
-//   styleUrls: ['./filter-prompt.component.css']
-// })
-// export class FilterPromptComponent implements OnInit {
-//   @Output() close = new EventEmitter<void>();
-//   @Output() applyFilters = new EventEmitter<any>();
-
-//   serviceTypes: string[] = [];
-//   eventTriggers: string[] = [];
-//   parties: string[] = [];
-
-//   selectedServiceType: string = '';
-//   selectedEventTrigger: string = '';
-//   selectedParty: string = '';
-
-//   startDate: string = '';
-//   endDate: string = '';
-
-//   constructor(private dataService: DataService) {}
-
-//   ngOnInit() {
-//     this.dataService.getFilteredServiceTypes().subscribe(data => {
-//       console.log('Service Types:', data);  // Debug log
-//       this.serviceTypes = data;
-//     });
-//   }
-
-//   onServiceTypeChange() {
-//     console.log('Selected Service Type:', this.selectedServiceType);  // Debug log
-//     if (this.selectedServiceType) {
-//       this.dataService.getFilteredEventTriggers(this.selectedServiceType).subscribe(data => {
-//         console.log('Event Triggers:', data);  // Debug log
-//         this.eventTriggers = data;
-//         this.selectedEventTrigger = '';
-//         this.parties = [];
-//         this.selectedParty = '';
-//       });
-//     } else {
-//       this.eventTriggers = [];
-//       this.parties = [];
-//       this.selectedEventTrigger = '';
-//       this.selectedParty = '';
-//     }
-//   }
-
-//   onEventTriggerChange() {
-//     console.log('Selected Event Trigger:', this.selectedEventTrigger);  // Debug log
-//     if (this.selectedEventTrigger) {
-//       this.dataService.getFilteredParties(this.selectedServiceType, this.selectedEventTrigger).subscribe(data => {
-//         console.log('Parties:', data);  // Debug log
-//         this.parties = data;
-//         this.selectedParty = '';
-//       });
-//     } else {
-//       this.parties = [];
-//       this.selectedParty = '';
-//     }
-//   }
-
-//   applyFilter() {
-//     const filterCriteria = {
-//       serviceType: this.selectedServiceType,
-//       eventTrigger: this.selectedEventTrigger,
-//       party: this.selectedParty,
-//       startDate: this.startDate,
-//       endDate: this.endDate
-//     };
-//     this.applyFilters.emit(filterCriteria);
-//     this.close.emit();
-//   }
-
-//   clearFilters() {
-//     this.selectedServiceType = '';
-//     this.selectedEventTrigger = '';
-//     this.selectedParty = '';
-//     this.startDate = '';
-//     this.endDate = '';
-//     this.eventTriggers = [];
-//     this.parties = [];
-//   }
-// }
-
-
-//new
-
 import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
 
@@ -184,7 +9,7 @@ import { DataService } from '../../services/data.service';
 export class FilterPromptComponent implements OnInit {
   @Output() closeDialog = new EventEmitter<void>();
   @Output() applyFilters = new EventEmitter<any>();
-  @Input() isVisible: boolean = false;  // Add this line
+  @Input() isVisible: boolean = false;
 
   serviceTypes: string[] = [];
   eventTriggers: string[] = [];
@@ -197,44 +22,106 @@ export class FilterPromptComponent implements OnInit {
   startDate: string = '';
   endDate: string = '';
 
+  serviceTypeSearch: string = '';
+  eventTriggerSearch: string = '';
+  partySearch: string = '';
+
+  filteredServiceTypes: string[] = [];
+  filteredEventTriggers: string[] = [];
+  filteredParties: string[] = [];
+
+  showServiceTypeDropdown: boolean = false;
+  showEventTriggerDropdown: boolean = false;
+  showPartyDropdown: boolean = false;
+
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
     this.dataService.getFilteredServiceTypes().subscribe(data => {
-      console.log('Service Types:', data);  // Debug log
       this.serviceTypes = data;
+      this.filteredServiceTypes = data; // Initialize filteredServiceTypes
     });
+  }
+  
+  filterServiceTypes() {
+    if (this.serviceTypeSearch.trim() === '') {
+      this.filteredServiceTypes = this.serviceTypes; // Show all options if the search input is empty
+    } else {
+      this.filteredServiceTypes = this.serviceTypes.filter(type =>
+        type.toLowerCase().includes(this.serviceTypeSearch.toLowerCase())
+      );
+    }
+  }
+  
+
+  filterEventTriggers() {
+    this.filteredEventTriggers = this.eventTriggers.filter(trigger =>
+      trigger.toLowerCase().includes(this.eventTriggerSearch.toLowerCase())
+    );
+  }
+
+  filterParties() {
+    this.filteredParties = this.parties.filter(party =>
+      party.toLowerCase().includes(this.partySearch.toLowerCase())
+    );
+  }
+
+  selectServiceType(serviceType: string) {
+    this.selectedServiceType = serviceType;
+    this.serviceTypeSearch = serviceType;
+    this.showServiceTypeDropdown = false;
+    this.onServiceTypeChange();
+  }
+
+  selectEventTrigger(eventTrigger: string) {
+    this.selectedEventTrigger = eventTrigger;
+    this.eventTriggerSearch = eventTrigger;
+    this.showEventTriggerDropdown = false;
+    this.onEventTriggerChange();
+  }
+
+  selectParty(party: string) {
+    this.selectedParty = party;
+    this.partySearch = party;
+    this.showPartyDropdown = false;
   }
 
   onServiceTypeChange() {
-    console.log('Selected Service Type:', this.selectedServiceType);  // Debug log
     if (this.selectedServiceType) {
       this.dataService.getFilteredEventTriggers(this.selectedServiceType).subscribe(data => {
-        console.log('Event Triggers:', data);  // Debug log
         this.eventTriggers = data;
+        this.filteredEventTriggers = data; // Initialize filteredEventTriggers
         this.selectedEventTrigger = '';
         this.parties = [];
         this.selectedParty = '';
+        this.eventTriggerSearch = '';
+        this.partySearch = '';
       });
     } else {
       this.eventTriggers = [];
+      this.filteredEventTriggers = [];
       this.parties = [];
+      this.filteredParties = [];
       this.selectedEventTrigger = '';
       this.selectedParty = '';
+      this.eventTriggerSearch = '';
+      this.partySearch = '';
     }
   }
 
   onEventTriggerChange() {
-    console.log('Selected Event Trigger:', this.selectedEventTrigger);  // Debug log
     if (this.selectedEventTrigger) {
       this.dataService.getFilteredParties(this.selectedServiceType, this.selectedEventTrigger).subscribe(data => {
-        console.log('Parties:', data);  // Debug log
         this.parties = data;
+        this.filteredParties = data; // Initialize filteredParties
         this.selectedParty = '';
+        this.partySearch = '';
       });
     } else {
       this.parties = [];
+      this.filteredParties = [];
       this.selectedParty = '';
+      this.partySearch = '';
     }
   }
 
@@ -256,7 +143,12 @@ export class FilterPromptComponent implements OnInit {
     this.selectedParty = '';
     this.startDate = '';
     this.endDate = '';
-    this.eventTriggers = [];
-    this.parties = [];
+    this.serviceTypeSearch = '';
+    this.eventTriggerSearch = '';
+    this.partySearch = '';
+    this.filteredServiceTypes = this.serviceTypes;
+    this.filteredEventTriggers = [];
+    this.filteredParties = [];
   }
 }
+
