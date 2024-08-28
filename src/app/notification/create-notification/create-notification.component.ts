f2218241557849a73f9489060b6d5ddab50a4d31
Sruti Dash <sruti.dash1203@gmail.com>
2024-08-28 13:46:51 +0530

    added language values over create new field

--- src/app/notification/create-notification/create-notification.component.ts
+++ src/app/notification/create-notification/create-notification.component.ts
@@ -1,3 +1,199 @@
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { DataService } from '../../services/data.service';
+
+// @Component({
+//   selector: 'app-create-notification',
+//   templateUrl: './create-notification.component.html',
+//   styleUrls: ['./create-notification.component.css']
+// })
+// export class CreateNotificationComponent implements OnInit {
+//   notification = {
+//     serviceType: '',
+//     eventTrigger: '',
+//     party: '',
+//     parameters: [
+//       { channel: '', language: 'English', header: '', body: '', documents: '', filteredChannels: [] }
+//     ]
+//   };
+
+//   serviceTypes: string[] = [];
+//   filteredServiceTypes: string[] = [];
+//   eventTriggers: string[] = [];
+//   filteredEventTriggers: string[] = [];
+//   parties: string[] = [];
+//   filteredParties: string[] = [];
+//   parameters: string[] = [];
+//   filteredParameters: string[] = [];
+//   channels: string[] = ['In-App Notification', 'SMS', 'Email', 'URL'];
+//   documents: string[] = ['Document 1', 'Document 2', 'Document 3'];
+
+//   showServiceTypeOptions: boolean = false;
+//   showEventTriggerOptions: boolean = false;
+//   showPartyOptions: boolean = false;
+//   showParameterOptions: boolean = false;
+
+//   constructor(private dataService: DataService, private router: Router) {}
+
+//   ngOnInit(): void {
+//     const navigation = this.router.getCurrentNavigation();
+//     if (navigation?.extras.state && navigation.extras.state['notification']) {
+//       this.notification = navigation.extras.state['notification'];
+//       this.populateFields();
+//     } else {
+//       this.fetchInitialData();
+//     }
+//   }
+  
+//   fetchInitialData(): void {
+//     this.dataService.getServiceTypes().subscribe((data: any) => {
+//       this.serviceTypes = data;
+//       this.filteredServiceTypes = this.serviceTypes;
+//     });
+  
+//     this.dataService.getEventTriggers().subscribe((data: any) => {
+//       this.eventTriggers = data;
+//       this.filteredEventTriggers = this.eventTriggers;
+//     });
+  
+//     this.dataService.getParties().subscribe((data: any) => {
+//       this.parties = data;
+//       this.filteredParties = this.parties;
+//     });
+  
+//     this.dataService.getCreateParameters().subscribe((data: any) => {
+//       this.parameters = data;
+//       this.filteredParameters = data;
+//     });
+//   }
+  
+//   populateFields(): void {
+//     // This method will populate the fields based on the notification data
+//     this.filteredServiceTypes = this.serviceTypes.filter(option =>
+//       option.toLowerCase().includes(this.notification.serviceType.toLowerCase())
+//     );
+//     this.filteredEventTriggers = this.eventTriggers.filter(option =>
+//       option.toLowerCase().includes(this.notification.eventTrigger.toLowerCase())
+//     );
+//     this.filteredParties = this.parties.filter(option =>
+//       option.toLowerCase().includes(this.notification.party.toLowerCase())
+//     );
+//     this.filteredParameters = this.parameters.filter(option =>
+//       option.toLowerCase().includes(this.notification.parameters[0].body.toLowerCase())
+//     );
+//   }
+  
+  
+
+//   filterOptions(type: string, event: any, param?: any) {
+//     const query = event.target.value.toLowerCase();
+//     if (type === 'serviceType') {
+//       this.filteredServiceTypes = this.serviceTypes.filter(option =>
+//         option.toLowerCase().includes(query)
+//       );
+//     } else if (type === 'eventTrigger') {
+//       this.filteredEventTriggers = this.eventTriggers.filter(option =>
+//         option.toLowerCase().includes(query)
+//       );
+//     } else if (type === 'party') {
+//       this.filteredParties = this.parties.filter(option =>
+//         option.toLowerCase().includes(query)
+//       );
+//     } else if (type === 'parameter') {
+//       this.filteredParameters = this.parameters.filter(option =>
+//         option.toLowerCase().includes(query)
+//       );
+//     } else if (type === 'channel' && param) {
+//       param.filteredChannels = this.channels.filter(option =>
+//         option.toLowerCase().includes(query)
+//       );
+//     }
+//   }
+
+//   selectOption(type: string, option: string, param?: any) {
+//     if (type === 'serviceType') {
+//       this.notification.serviceType = option;
+//       this.showServiceTypeOptions = false;
+//       this.filteredServiceTypes = [];
+//       (document.getElementById('serviceTypeSearch') as HTMLInputElement).value = option;
+//     } else if (type === 'eventTrigger') {
+//       this.notification.eventTrigger = option;
+//       this.showEventTriggerOptions = false;
+//       this.filteredEventTriggers = [];
+//       (document.getElementById('eventTriggerSearch') as HTMLInputElement).value = option;
+//     } else if (type === 'party') {
+//       this.notification.party = option;
+//       this.showPartyOptions = false;
+//       this.filteredParties = [];
+//       (document.getElementById('partySearch') as HTMLInputElement).value = option;
+//     } else if (type === 'parameter') {
+//       this.notification.parameters[0].body = option;
+//       this.notification.parameters[0].header = this.notification.eventTrigger;
+//       this.showParameterOptions = false;
+//       this.filteredParameters = [];
+//       (document.getElementById('parameterSearch') as HTMLInputElement).value = option;
+//       this.checkFormValidity();
+//     } else if (type === 'channel' && param) {
+//       param.channel = option;
+//       param.filteredChannels = [];
+//       this.checkFormValidity();
+//     }
+//   }
+
+//   showOptions(type: string, param?: any) {
+//     if (type === 'serviceType') {
+//       this.showServiceTypeOptions = true;
+//       this.filteredServiceTypes = [...this.serviceTypes];
+//     } else if (type === 'eventTrigger') {
+//       this.showEventTriggerOptions = true;
+//       this.filteredEventTriggers = [...this.eventTriggers];
+//     } else if (type === 'party') {
+//       this.showPartyOptions = true;
+//       this.filteredParties = [...this.parties];
+//     } else if (type === 'parameter') {
+//       this.showParameterOptions = true;
+//       this.filteredParameters = [...this.parameters];
+//     } else if (type === 'channel' && param) {
+//       param.filteredChannels = [...this.channels];
+//     }
+//   }
+
+//   checkFormValidity() {
+//     return (
+//       this.notification.serviceType !== '' &&
+//       this.notification.eventTrigger !== '' &&
+//       this.notification.party !== '' &&
+//       this.notification.parameters[0].body !== '' &&
+//       this.notification.parameters[0].channel !== ''
+//     );
+//   }
+
+//   isFormValid(): boolean {
+//     return this.checkFormValidity();
+//   }
+
+//   cancel() {
+//     this.router.navigate(['/notification-management']);
+//   }
+
+//   next() {
+//     if (this.isFormValid()) {
+//       // Set the notification data in the data service
+//       this.dataService.setNotification(this.notification);
+
+//       // Navigate to the Notification Review page with the notification data
+//       this.router.navigate(['/notification-review'], {
+//         state: { notification: this.notification },
+//       });
+//     } else {
+//       console.error('Form is not valid.');
+//     }
+//   }
+// }
+
+
+//new
+
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../../services/data.service';
@@ -13,7 +209,7 @@ export class CreateNotificationComponent implements OnInit {
     eventTrigger: '',
     party: '',
     parameters: [
-      { channel: '', language: 'English', header: '', body: '', documents: '', filteredChannels: [] }
+      { channel: '', language: '', header: '', body: '', documents: '', filteredChannels: [] }
     ]
   };
 
@@ -25,6 +221,8 @@ export class CreateNotificationComponent implements OnInit {
   filteredParties: string[] = [];
   parameters: string[] = [];
   filteredParameters: string[] = [];
+  languages: string[] = [];
+  filteredLanguages: string[] = [];
   channels: string[] = ['In-App Notification', 'SMS', 'Email', 'URL'];
   documents: string[] = ['Document 1', 'Document 2', 'Document 3'];
 
@@ -32,6 +230,7 @@ export class CreateNotificationComponent implements OnInit {
   showEventTriggerOptions: boolean = false;
   showPartyOptions: boolean = false;
   showParameterOptions: boolean = false;
+  showLanguageOptions: boolean = false;
 
   constructor(private dataService: DataService, private router: Router) {}
 
@@ -44,31 +243,8 @@ export class CreateNotificationComponent implements OnInit {
       this.fetchInitialData();
     }
   }
-  
-  fetchInitialData(): void {
-    this.dataService.getServiceTypes().subscribe((data: any) => {
-      this.serviceTypes = data;
-      this.filteredServiceTypes = this.serviceTypes;
-    });
-  
-    this.dataService.getEventTriggers().subscribe((data: any) => {
-      this.eventTriggers = data;
-      this.filteredEventTriggers = this.eventTriggers;
-    });
-  
-    this.dataService.getParties().subscribe((data: any) => {
-      this.parties = data;
-      this.filteredParties = this.parties;
-    });
-  
-    this.dataService.getCreateParameters().subscribe((data: any) => {
-      this.parameters = data;
-      this.filteredParameters = data;
-    });
-  }
-  
+
   populateFields(): void {
-    // This method will populate the fields based on the notification data
     this.filteredServiceTypes = this.serviceTypes.filter(option =>
       option.toLowerCase().includes(this.notification.serviceType.toLowerCase())
     );
@@ -82,8 +258,33 @@ export class CreateNotificationComponent implements OnInit {
       option.toLowerCase().includes(this.notification.parameters[0].body.toLowerCase())
     );
   }
-  
-  
+
+  fetchInitialData(): void {
+    this.dataService.getServiceTypes().subscribe((data: any) => {
+      this.serviceTypes = data;
+      this.filteredServiceTypes = this.serviceTypes;
+    });
+
+    this.dataService.getEventTriggers().subscribe((data: any) => {
+      this.eventTriggers = data;
+      this.filteredEventTriggers = this.eventTriggers;
+    });
+
+    this.dataService.getParties().subscribe((data: any) => {
+      this.parties = data;
+      this.filteredParties = this.parties;
+    });
+
+    this.dataService.getCreateParameters().subscribe((data: any) => {
+      this.parameters = data;
+      this.filteredParameters = data;
+    });
+
+    this.dataService.getLanguages().subscribe((languages: string[]) => {
+      this.languages = languages;
+      this.filteredLanguages = this.languages;
+    });
+  }
 
   filterOptions(type: string, event: any, param?: any) {
     const query = event.target.value.toLowerCase();
@@ -105,6 +306,10 @@ export class CreateNotificationComponent implements OnInit {
       );
     } else if (type === 'channel' && param) {
       param.filteredChannels = this.channels.filter(option =>
+        option.toLowerCase().includes(query)
+      );
+    } else if (type === 'language') {
+      this.filteredLanguages = this.languages.filter(option =>
         option.toLowerCase().includes(query)
       );
     }
@@ -137,6 +342,11 @@ export class CreateNotificationComponent implements OnInit {
       param.channel = option;
       param.filteredChannels = [];
       this.checkFormValidity();
+    } else if (type === 'language' && param) {
+      param.language = option;
+      this.showLanguageOptions = false;
+      this.filteredLanguages = [];
+      this.checkFormValidity();
     }
   }
 
@@ -155,6 +365,9 @@ export class CreateNotificationComponent implements OnInit {
       this.filteredParameters = [...this.parameters];
     } else if (type === 'channel' && param) {
       param.filteredChannels = [...this.channels];
+    } else if (type === 'language') {
+      this.showLanguageOptions = true;
+      this.filteredLanguages = [...this.languages];
     }
   }
 
@@ -164,7 +377,8 @@ export class CreateNotificationComponent implements OnInit {
       this.notification.eventTrigger !== '' &&
       this.notification.party !== '' &&
       this.notification.parameters[0].body !== '' &&
-      this.notification.parameters[0].channel !== ''
+      this.notification.parameters[0].channel !== '' &&
+      this.notification.parameters[0].language !== ''
     );
   }
 
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,3 +1,230 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient, HttpParams } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class DataService {
+//   private baseUrl = 'http://localhost:5000/api';
+
+//   // Properties to hold the selected values
+//   selectedServiceType: string = '';
+//   selectedEventTrigger: string = '';
+//   selectedParty: string = '';
+//   senderParent: string = '';
+//   grade: string = '';
+//   channels: string[] = [];
+//   inAppNotifications: any[] = [];
+//   smsNotifications: any[] = [];
+//   emailNotifications: any[] = [];
+
+//   private notification: any = {};  // Store the edited notification
+
+//   constructor(private http: HttpClient) {}
+
+//   // Method to get service types
+//   getServiceTypes(): Observable<any> {
+//     return this.http.get(`${this.baseUrl}/service-types`);
+//   }
+
+//   // Method to get event triggers
+//   getEventTriggers(): Observable<any> {
+//     return this.http.get(`${this.baseUrl}/event-triggers`);
+//   }
+
+//   // Method to get parties
+//   getParties(): Observable<any> {
+//     return this.http.get(`${this.baseUrl}/parties`);
+//   }
+
+//   getCreateParameters(): Observable<string[]> {
+//     return this.http.get<string[]>(`${this.baseUrl}/create-parameters`);
+//   }
+
+// getEditParameters(serviceCode: string, eventName: string, partyType: string): Observable<string[]> {
+//   const params = new HttpParams()
+//       .set('serviceCode', serviceCode)
+//       .set('eventName', eventName)
+//       .set('partyType', partyType);
+
+//   return this.http.get<string[]>(`${this.baseUrl}/edit-parameters`, { params });
+// }
+
+
+
+//   // Method to delete a notification
+//   deleteNotification(id: number): Observable<any> {
+//     return this.http.delete(`${this.baseUrl}/notification-templates/${id}`);
+//   }
+
+//   // Method to get filtered service types
+//   getFilteredServiceTypes(): Observable<any> {
+//     return this.http.get(`${this.baseUrl}/filter-service-types`);
+//   }
+
+//   // Method to get filtered event triggers
+//   getFilteredEventTriggers(serviceType: string): Observable<any> {
+//     return this.http.get(`${this.baseUrl}/filter-event-triggers/${serviceType}`);
+//   }
+
+//   // Method to get filtered parties
+//   getFilteredParties(serviceType: string, eventTrigger: string): Observable<any> {
+//     return this.http.get(`${this.baseUrl}/filter-parties/${serviceType}/${eventTrigger}`);
+//   }
+
+//   // Method to get notification templates with filters
+//   getNotificationTemplates(filters: { [key: string]: string } = {}): Observable<any[]> {
+//     let params = new HttpParams();
+//     for (const key in filters) {
+//       if (filters[key]) {
+//         params = params.append(key, filters[key]);
+//       }
+//     }
+//     return this.http.get<any[]>(`${this.baseUrl}/notification-templates`, { params });
+//   }
+
+//   // Method to get notification details
+//   getNotificationDetails(id: number): Observable<any> {
+//     return this.http.get<any>(`${this.baseUrl}/notification-details/${id}`);
+//   }
+
+//   // Method to set the notification object
+//   setNotification(notification: any) {
+//     this.notification = notification;
+//   }
+
+//   // Method to get the notification object
+//   getNotification() {
+//     return this.notification;
+//   }
+
+//   // Method to save a notification
+//   saveNotification(): Observable<any> {
+//     return this.http.post(`${this.baseUrl}/save-notification`, {
+//       serviceType: this.selectedServiceType,
+//       eventTrigger: this.selectedEventTrigger,
+//       party: this.selectedParty,
+//       senderParent: this.senderParent,
+//       grade: this.grade,
+//       channels: this.channels,
+//       inAppNotifications: this.inAppNotifications,
+//       smsNotifications: this.smsNotifications,
+//       emailNotifications: this.emailNotifications
+//     });
+//   }
+// }
+
+
+//new
+
+// import { Injectable } from '@angular/core';
+// import { HttpClient, HttpParams } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class DataService {
+//   private baseUrl = 'http://localhost:5000/api';
+
+//   private notification: any = {};  // Store the edited notification
+
+//   constructor(private http: HttpClient) {}
+
+//   // Method to get service types
+//   getServiceTypes(): Observable<any> {
+//     return this.http.get(`${this.baseUrl}/service-types`);
+//   }
+
+//   // Method to get event triggers
+//   getEventTriggers(): Observable<any> {
+//     return this.http.get(`${this.baseUrl}/event-triggers`);
+//   }
+
+//   // Method to get parties
+//   getParties(): Observable<any> {
+//     return this.http.get(`${this.baseUrl}/parties`);
+//   }
+
+//   // Method to get parameters for create notification layout
+//   getCreateParameters(): Observable<string[]> {
+//     return this.http.get<string[]>(`${this.baseUrl}/create-parameters`);
+//   }
+
+//   // Method to get languages for the dropdown
+//   getLanguages(): Observable<string[]> {
+//     return this.http.get<string[]>(`${this.baseUrl}/create-new-language`);
+//   }
+
+//   // Method to get parameters for edit notification layout
+//   getEditParameters(serviceCode: string, eventName: string, partyType: string): Observable<string[]> {
+//     const params = new HttpParams()
+//       .set('serviceCode', serviceCode)
+//       .set('eventName', eventName)
+//       .set('partyType', partyType);
+
+//     return this.http.get<string[]>(`${this.baseUrl}/edit-parameters`, { params });
+//   }
+
+//   // Method to delete a notification
+//   deleteNotification(id: number): Observable<any> {
+//     return this.http.delete(`${this.baseUrl}/notification-templates/${id}`);
+//   }
+
+//   // Method to get filtered service types
+//   getFilteredServiceTypes(): Observable<any> {
+//     return this.http.get(`${this.baseUrl}/filter-service-types`);
+//   }
+
+//   // Method to get filtered event triggers
+//   getFilteredEventTriggers(serviceType: string): Observable<any> {
+//     return this.http.get(`${this.baseUrl}/filter-event-triggers/${serviceType}`);
+//   }
+
+//   // Method to get filtered parties
+//   getFilteredParties(serviceType: string, eventTrigger: string): Observable<any> {
+//     return this.http.get(`${this.baseUrl}/filter-parties/${serviceType}/${eventTrigger}`);
+//   }
+
+//   // Method to get notification templates with filters
+//   getNotificationTemplates(filters: { [key: string]: string } = {}): Observable<any[]> {
+//     let params = new HttpParams();
+//     for (const key in filters) {
+//       if (filters[key]) {
+//         params = params.append(key, filters[key]);
+//       }
+//     }
+//     return this.http.get<any[]>(`${this.baseUrl}/notification-templates`, { params });
+//   }
+
+//   // Method to get notification details
+//   getNotificationDetails(id: number): Observable<any> {
+//     return this.http.get<any>(`${this.baseUrl}/notification-details/${id}`);
+//   }
+
+//   // Method to set the notification object
+//   setNotification(notification: any) {
+//     this.notification = notification;
+//   }
+
+//   // Method to get the notification object
+//   getNotification() {
+//     return this.notification;
+//   }
+
+//   // Method to save a notification
+//   saveNotification(): Observable<any> {
+//     return this.http.post(`${this.baseUrl}/save-notification`, {
+//       serviceType: this.notification.serviceType,
+//       eventTrigger: this.notification.eventTrigger,
+//       party: this.notification.party,
+//       parameters: this.notification.parameters
+//     });
+//   }
+// }
+
+
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -38,20 +265,25 @@ export class DataService {
     return this.http.get(`${this.baseUrl}/parties`);
   }
 
+  // Method to get parameters for create notification layout
   getCreateParameters(): Observable<string[]> {
     return this.http.get<string[]>(`${this.baseUrl}/create-parameters`);
   }
 
-getEditParameters(serviceCode: string, eventName: string, partyType: string): Observable<string[]> {
-  const params = new HttpParams()
+  // Method to get languages for the dropdown
+  getLanguages(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.baseUrl}/create-new-language`);
+  }
+
+  // Method to get parameters for edit notification layout
+  getEditParameters(serviceCode: string, eventName: string, partyType: string): Observable<string[]> {
+    const params = new HttpParams()
       .set('serviceCode', serviceCode)
       .set('eventName', eventName)
       .set('partyType', partyType);
 
-  return this.http.get<string[]>(`${this.baseUrl}/edit-parameters`, { params });
-}
-
-
+    return this.http.get<string[]>(`${this.baseUrl}/edit-parameters`, { params });
+  }
 
   // Method to delete a notification
   deleteNotification(id: number): Observable<any> {
@@ -92,6 +324,16 @@ getEditParameters(serviceCode: string, eventName: string, partyType: string): Ob
   // Method to set the notification object
   setNotification(notification: any) {
     this.notification = notification;
+    // Set individual properties
+    this.selectedServiceType = notification.serviceType;
+    this.selectedEventTrigger = notification.eventTrigger;
+    this.selectedParty = notification.party;
+    this.senderParent = notification.senderParent || '';
+    this.grade = notification.grade || '';
+    this.channels = notification.channels || [];
+    this.inAppNotifications = notification.inAppNotifications || [];
+    this.smsNotifications = notification.smsNotifications || [];
+    this.emailNotifications = notification.emailNotifications || [];
   }
 
   // Method to get the notification object
